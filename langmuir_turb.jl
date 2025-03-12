75e0e0900e10569dd2f38457056c1766c4929992
anpa5279 <anpa5279@colorado.edu>
2025-03-12 11:34:08 -0600

    attempting to fixed errors

--- langmuir_turb.jl
+++ langmuir_turb.jl
@@ -39,9 +39,9 @@ println("Hello from process $rank out of $Nranks")
 grid = RectilinearGrid(arch; size=(params.Nx, params.Ny, params.Nz), extent=(params.Lx, params.Ly, params.Lz))
 @show grid
 
-buoyancy = SeawaterBuoyancy(equation_of_state=LinearEquationOfState(thermal_expansion = 2e-4,
-                                                                    haline_contraction = 8e-4))
-@show buoyancy
+#buoyancy = SeawaterBuoyancy(equation_of_state=LinearEquationOfState(thermal_expansion = 2e-4,
+#                                                                    haline_contraction = 8e-4))
+#@show buoyancy
 
 T_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(params.Q / (params.ρₒ * params.cᴾ)),
                                 bottom = GradientBoundaryCondition(params.dTdz))
@@ -63,19 +63,20 @@ const Uˢ = params.amplitude^2 * wavenumber * frequency # m s⁻¹
 u_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(params.τx))
 @show u_bcs
 
-B_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(params.Jᵇ),
-                                                bottom = GradientBoundaryCondition(params.N²))
-@show B_bcs
+#B_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(params.Jᵇ),
+#                                                bottom = GradientBoundaryCondition(params.N²))
+#@show B_bcs
 
 coriolis = FPlane(f=1e-4)
 
-model = NonhydrostaticModel(; grid, buoyancy, coriolis,
+model = NonhydrostaticModel(; grid,coriolis,
                             advection = WENO(),
                             timestepper = :RungeKutta3,
+                            buoyancy = SeawaterBuoyancy(),
                             tracers = (:T, :b),
                             closure = AnisotropicMinimumDissipation(),
                             stokes_drift = UniformStokesDrift(∂z_uˢ=∂z_uˢ),
-                            boundary_conditions = (u=u_bcs, T=T_bcs, b=B_bcs))
+                            boundary_conditions = (u=u_bcs, T=T_bcs)) #B=B_bcs))
 @show model
 
 @inline Ξ(z) = randn() * exp(z / 4)
