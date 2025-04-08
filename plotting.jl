466df8b8f2218eaf71902432de8106710c87f7ad
anpa5279 <anpa5279@colorado.edu>
2025-04-08 16:46:11 -0600

    updating plotting and outputs

--- plotting.jl
+++ plotting.jl
@@ -5,6 +5,51 @@ using Printf
 using Oceananigans
 using Oceananigans.Units: minute, minutes, hours
 using Oceananigans.BuoyancyFormulations: g_Earth
+mutable struct Params
+    Nx::Int         # number of points in each of x direction
+    Ny::Int         # number of points in each of y direction
+    Nz::Int         # number of points in the vertical direction
+    Lx::Float64     # (m) domain horizontal extents
+    Ly::Float64     # (m) domain horizontal extents
+    Lz::Float64     # (m) domain depth 
+    N²::Float64     # s⁻², initial and bottom buoyancy gradient
+    initial_mixed_layer_depth::Float64 # m 
+    Q::Float64      # W m⁻², surface heat flux. cooling is positive
+    cᴾ::Float64     # J kg⁻¹ K⁻¹, specific heat capacity of seawater
+    ρₒ::Float64     # kg m⁻³, average density at the surface of the world ocean
+    dTdz::Float64   # K m⁻¹, temperature gradient
+    T0::Float64     # C, temperature at the surface   
+    β::Float64      # 1/K, thermal expansion coefficient
+    u₁₀::Float64    # (m s⁻¹) wind speed at 10 meters above the ocean
+    La_t::Float64   # Langmuir turbulence number
+end
+
+#defaults, these can be changed directly below 128, 128, 160, 320.0, 320.0, 96.0
+p = Params(32, 32, 32, 128, 128, 96.0, 5.3*(10^(-9)), 33.0, 5.0, 3991.0, 1000.0, 0.006667, 17.0, 2.0e-4, 5.75, 0.29)
+
+#functions
+function fluctuation(a, a_avg)
+    a_fluct = Array{Float64}(undef, size(a_avg))
+    for i in 1:size(a, 1)
+        for j in 1:size(a, 2)
+            for k in 1:size(a, 3)
+                a_fluct[i, j, k] = a[i, j, k] - a_avg[k]
+            end
+        end
+    end
+    return a_fluct
+end 
+function fluct_squared(a, a_avg)
+    a_fluct = Array{Float64}(undef, size(a_avg))
+    for i in 1:size(a, 1)
+        for j in 1:size(a, 2)
+            for k in 1:size(a, 3)
+                a_fluct2[i, j, k] = (a[i, j, k] - a_avg[k])^2
+            end
+        end
+    end
+    return a_fluct2
+end
 
 # running locally: using Pkg; Pkg.add("Oceananigans"); Pkg.add("CairoMakie"); Pkg.add("JLD2")
 Nranks = 4
@@ -14,8 +59,7 @@ averages_file="outputs/langmuir_turbulence_averages_0.jld2"
 
 w_temp = FieldTimeSeries(fld_file, "w")
 u_temp = FieldTimeSeries(fld_file, "u")
-T_temp = FieldTimeSeries(fld_file, "T")
-S_temp = FieldTimeSeries(fld_file, "S")
+B_temp = FieldTimeSeries(fld_file, "B")
 U_temp = FieldTimeSeries(averages_file, "U")
 V_temp = FieldTimeSeries(averages_file, "V")
 wu_temp = FieldTimeSeries(averages_file, "wu")
@@ -33,8 +77,7 @@ times = u_temp.times
 
 w_data = Array{Float64}(undef, (Nx, Ny, Nz + 1, Nt)) #because face value
 u_data = Array{Float64}(undef, (Nx, Ny, Nz, Nt))
-T_data = Array{Float64}(undef, (Nx, Ny, Nz, Nt))
-S_data = Array{Float64}(undef, (Nx, Ny, Nz, Nt))
+B_data = Array{Float64}(undef, (Nx, Ny, Nz, Nt))
 U_data = Array{Float64}(undef, (1, 1, Nz, Nt))
 V_data = Array{Float64}(undef, (1, 1, Nz, Nt))
 wu_data = Array{Float64}(undef, (1, 1, Nz + 1, Nt))  
@@ -47,8 +90,7 @@ wv_data .= 0
 p = 1
 w_data[p:p + w_temp.grid.Nx - 1, :, :, :] .= w_temp.data
 u_data[p:p + u_temp.grid.Nx - 1, :, :, :] .= u_temp.data
-T_data[p:p + T_temp.grid.Nx - 1, :, :, :] .= T_temp.data
-S_data[p:p + S_temp.grid.Nx - 1, :, :, :] .= S_temp.data
+B_data .= B_data .+ B_temp.data
 U_data .= U_data .+ U_temp.data
 V_data .= V_data .+ V_temp.data
 wu_data .= wu_data .+ wu_temp.data
@@ -65,8 +107,7 @@ for i in 1:Nranks-1
 
     w_temp = FieldTimeSeries(fld_file, "w")
     u_temp = FieldTimeSeries(fld_file, "u")
-    T_temp = FieldTimeSeries(fld_file, "T")
-    S_temp = FieldTimeSeries(fld_file, "S")
+    B_temp = FieldTimeSeries(fld_file, "B")
     U_temp = FieldTimeSeries(averages_file, "U")
     V_temp = FieldTimeSeries(averages_file, "V")
     wu_temp = FieldTimeSeries(averages_file, "wu")
@@ -74,8 +115,7 @@ for i in 1:Nranks-1
     
     w_data[p:p + w_temp.grid.Nx - 1, :, :, :] .= w_temp.data
     u_data[p:p + u_temp.grid.Nx - 1, :, :, :] .= u_temp.data
-    T_data[p:p + T_temp.grid.Nx - 1, :, :, :] .= T_temp.data
-    S_data[p:p + S_temp.grid.Nx - 1, :, :, :] .= S_temp.data
+    B_data[p:p + w_temp.grid.Nx - 1, :, :, :] .= B_temp.data
     U_data .= U_data .+ U_temp.data
     V_data .= V_data .+ V_temp.data
     wu_data .= wu_data .+ wu_temp.data
@@ -84,6 +124,7 @@ for i in 1:Nranks-1
 end
 
 #averaging
+B_data = B_data ./ Nranks
 U_data = U_data ./ Nranks
 V_data = V_data ./ Nranks
 wu_data = wu_data ./ Nranks
@@ -99,7 +140,6 @@ B_data = Statistics.mean(B_temp, dims=(1, 2))
 #putting everything back into FieldTimeSeries
 w = FieldTimeSeries{Center, Center, Face}(grid, times)
 u = FieldTimeSeries{Face, Center, Center}(grid, times)
-T = FieldTimeSeries{Face, Center, Center}(grid, times)
 S = FieldTimeSeries{Face, Center, Center}(grid, times)
 B = FieldTimeSeries{Center, Center, Center}(grid, times)
 U = FieldTimeSeries{Center, Center, Center}(grid, times)
@@ -119,108 +159,109 @@ wv .= wv_data
 
 #begin plotting
 n = Observable(1)
-
-wxy_title = @lift string("w(x, y, t), t = ", prettytime(times[$n]))
-wxz_title = @lift string("w(x, z, t), t = ", prettytime(times[$n]))
-uxz_title = @lift string("u(x, z, t), t = ", prettytime(times[$n]))
-
-xT, yT, zT = nodes(T)
-
 axis_kwargs = (xlabel="x (m)",
             ylabel="z (m)",
-            aspect = AxisAspect(Lx/Lz),
-            limits = ((0, Lx), (-Lz, 0)))
-
+            aspect = AxisAspect(p.Lx/p.Lz),
+            limits = ((0, p.Lx), (-p.Lz, 0)))
 fig = Figure(size = (850, 1150))
+title = @lift @sprintf("t = %s", prettytime(times[$n]))
 
-ax_B = Axis(fig[1, 4:5];
-            xlabel = "Buoyancy (m s⁻²)", xtickformat = "{:.3f}", xticklabelrotation = pi/4,
-            ylabel = "z (m)")
-
-ax_U = Axis(fig[2, 4:5];
-            xlabel = "Velocities (m s⁻¹)",
-            ylabel = "z (m)",
-            limits = ((-0.07, 0.07), nothing))
-
-ax_fluxes = Axis(fig[3, 4:5];
-                xlabel = "Momentum fluxes (m² s⁻²)", xticklabelrotation = pi/4,
-                ylabel = "z (m)",
-                limits = ((-3.5e-5, 3.5e-5), nothing))
-
+# w surface plane slice
+wxy_title = @lift string("w(x, y, t), t = ", prettytime(times[$n]))
 ax_wxy = Axis(fig[1, 1:2];
             xlabel = "x (m)",
             ylabel = "y (m)",
             aspect = DataAspect(),
-            limits = ((0, Lx), (0, Ly)),
+            limits = ((0, p.Lx), (0, p.Ly)),
             title = wxy_title)
-
-ax_wxz = Axis(fig[2, 1:2]; title = wxz_title, axis_kwargs...)
-
-ax_uxz = Axis(fig[3, 1:2]; title = uxz_title, axis_kwargs...)
-
-ax_T  = Axis(fig[4, 1:2]; title = "Temperature", axis_kwargs...)
-
-ax_S  = Axis(fig[4, 4:5]; title = "Salinity", axis_kwargs...)
-title = @lift @sprintf("t = %s", prettytime(times[$n]))
-
-wₙ = @lift w[$n]
-uₙ = @lift u[$n]
-Tₙ = @lift interior(T[$n],  :, 1, :)
-Sₙ = @lift interior(S[$n],  :, 1, :)
-Bₙ = @lift view(B[$n], 1, 1, :)
-Uₙ = @lift view(U[$n], 1, 1, :)
-Vₙ = @lift view(V[$n], 1, 1, :)
-wuₙ = @lift view(wu[$n], 1, 1, :)
-wvₙ = @lift view(wv[$n], 1, 1, :)
-
-k = searchsortedfirst(znodes(grid, Face(); with_halos=true), -2)
+k = searchsortedfirst(znodes(grid, Face(); with_halos=false), -2)
 wxyₙ = @lift view(w[$n], :, :, k)
-wxzₙ = @lift view(w[$n], :, Ny/2, :)
-uxzₙ = @lift view(u[$n], :, Ny/2, :)
-
-wlims = (-0.03, 0.03)
-ulims = (-0.05, 0.05)
-Tlims = (19.7, 19.99)
-Slims = (35, 35.005)
-
-lines!(ax_B, Bₙ)
-
-lines!(ax_U, Uₙ; label = L"\bar{u}")
-lines!(ax_U, Vₙ; label = L"\bar{v}")
-axislegend(ax_U; position = :rb)
-
-lines!(ax_fluxes, wuₙ; label = L"mean $wu$")
-lines!(ax_fluxes, wvₙ; label = L"mean $wv$")
-axislegend(ax_fluxes; position = :rb)
-
-hm_T = heatmap!(ax_T, xT, zT, Tₙ; colormap = :thermal, colorrange = Tlims)
-Colorbar(fig[4, 3], hm_T; label = "ᵒC")
-
+wlims = (-0.02, 0.02)
 hm_wxy = heatmap!(ax_wxy, wxyₙ;
                 colorrange = wlims,
                 colormap = :balance)
-
 Colorbar(fig[1, 3], hm_wxy; label = "m s⁻¹")
 
+# w xz plane slice
+wxz_title = @lift string("w(x, z, t), t = ", prettytime(times[$n]))
+ax_wxz = Axis(fig[2, 1:2]; title = wxz_title, axis_kwargs...)
+wxzₙ = @lift view(w[$n], :, p.Ny/2, :)
 hm_wxz = heatmap!(ax_wxz, wxzₙ;
                 colorrange = wlims,
                 colormap = :balance)
 
 Colorbar(fig[2, 3], hm_wxz; label = "m s⁻¹")
 
+# w yz plane slice
+wyz_title = @lift string("w(y, z, t), t = ", prettytime(times[$n]))
+ax_wxz = Axis(fig[3, 1:2]; title = wyz_title, xlabel = "y (m)",
+            ylabel = "z (m)",
+            aspect = DataAspect(),
+            limits = ((0, p.Ly), (-p.Lz, 0)))
+wxzₙ = @lift view(w[$n], p.Nx/2, :, :)
+hm_wxz = heatmap!(ax_wxz, wxzₙ;
+                colorrange = wlims,
+                colormap = :balance)
+
+Colorbar(fig[3, 3], hm_wxz; label = "m s⁻¹")
+
+# u xz plane slice
+uxz_title = @lift string("u(x, z, t), t = ", prettytime(times[$n]))
+ax_uxz = Axis(fig[4, 1:2]; title = uxz_title, axis_kwargs...)
+uₙ = @lift u[$n]
+uxzₙ = @lift view(u[$n], :, p.Ny/2, :)
+ulims = (-0.06, 0.06)
 ax_uxz = heatmap!(ax_uxz, uxzₙ;
                 colorrange = ulims,
                 colormap = :balance)
 
-Colorbar(fig[3, 3], ax_uxz; label = "m s⁻¹")
+Colorbar(fig[4, 3], ax_uxz; label = "m s⁻¹")
 
-hm_S = heatmap!(ax_S, xT, zT, Sₙ; colormap = :haline, colorrange = Slims)
-Colorbar(fig[4, 6], hm_S; label = "g / kg")
+# temperature vertical plane slice
+xT, yT, zT = nodes(T)
+T_title = @lift string("T(x, z, t), t = ", prettytime(times[$n]))   
+ax_T  = Axis(fig[4, 4:5]; title = T_title, xlabel = "y (m)",
+            ylabel = "z (m)",
+            aspect = DataAspect(),
+            limits = ((0, p.Ly), (-p.Lz, 0)))
+Tₙ = @lift interior(T[$n],  1, :, :)
+Tlims = (16.5, 17.0)
+hm_T = heatmap!(ax_T, xT, zT, Tₙ; colormap = :thermal, colorrange = Tlims)
+Colorbar(fig[4, 6], hm_T; label = "ᵒC")
+
+# buoyancy with depth
+ax_B = Axis(fig[1, 4:5];
+xlabel = "Buoyancy (m s⁻²)",
+ylabel = "z (m)")
+Bₙ = @lift view(B[$n], 1, 1, :)
+lines!(ax_B, Bₙ)
+
+# mean horizontal velocities with depth
+ax_U = Axis(fig[2, 4:5];
+xlabel = "Velocities (m s⁻¹)",
+ylabel = "z (m)",
+limits = ((-0.07, 0.07), nothing))
+Uₙ = @lift view(U[$n], 1, 1, :)
+Vₙ = @lift view(V[$n], 1, 1, :)
+lines!(ax_U, Uₙ; label = L"\bar{u}")
+lines!(ax_U, Vₙ; label = L"\bar{v}")
+axislegend(ax_U; position = :rb)
+
+# momentum fluxes with depth
+ax_fluxes = Axis(fig[3, 4:5];
+xlabel = "Momentum fluxes (m² s⁻²)",
+ylabel = "z (m)",
+limits = ((-3.5e-5, 3.5e-5), nothing))
+wuₙ = @lift view(wu[$n], 1, 1, :)
+wvₙ = @lift view(wv[$n], 1, 1, :)
+lines!(ax_fluxes, wuₙ; label = L"mean $wu$")
+lines!(ax_fluxes, wvₙ; label = L"mean $wv$")
+axislegend(ax_fluxes; position = :rb)
 
 fig
 
 frames = 1:length(times)
 
-record(fig, "langmuir_turbulence_temp_sal.mp4", frames, framerate=8) do i
+record(fig, "langmuir_turbulence_comparison.mp4", frames, framerate=8) do i
     n[] = i
 end
