1d489b325e4df460470f5d510b38b49d9256ba0c
anpa5279 <anpa5279@colorado.edu>
2025-04-21 10:48:10 -0600

    removing memory so there is no issue with RAM locally

--- plotting.jl
+++ plotting.jl
@@ -97,7 +97,7 @@ function plot()
         u_all = [f["timeseries"]["u"][t][xrange, yrange, urange] for t in t_save]
         b_all = [f["timeseries"]["b"][t][xrange, yrange, urange] for t in t_save]
         for k in 1:Nt
-            #@show k
+            @show k
             times[k] = f["timeseries"]["t"][t_save[k]]
             local w = w_all[k]
             local u = u_all[k]
@@ -106,19 +106,27 @@ function plot()
             w_data[nn:nn + Nr - 1, :, :, k] = w
             u_data[nn:nn + Nr - 1, :, :, k] = u
             b_data[nn:nn + Nr - 1, :, :, k] = b
+            #removing the data from memory
+            w = nothing
+            u = nothing
+            b = nothing
+            GC.gc()
         end
         U_data .= U_data .+ U_temp.data[:, :, 1:p.Nz, :]
         V_data .= V_data .+ V_temp.data[:, :, 1:p.Nz, :]
         wu_data .= wu_data .+ wu_temp.data[:, :, 1:p.Nz, :]
         wv_data .= wv_data .+ wv_temp.data[:, :, 1:p.Nz, :]
+        #removing the data from memory
         w_all = nothing
         u_all = nothing
         b_all = nothing
+        GC.gc() 
         close(f)
         close(fa)
     end
 
     #averaging
+    println("Averaging data")
     B_avg = b_data ./ (p.Nx * p.Ny * p.Nz)
     U_data = U_data ./ Nranks
     V_data = V_data ./ Nranks
@@ -126,6 +134,7 @@ function plot()
     wv_data = wv_data ./ Nranks
 
     #putting everything back into FieldTimeSeries
+    println("Putting data into FieldTimeSeries")
     w = FieldTimeSeries{Center, Center, Face}(grid, times)
     u = FieldTimeSeries{Face, Center, Center}(grid, times)
     b = FieldTimeSeries{Center, Center, Center}(grid, times)
@@ -144,110 +153,18 @@ function plot()
     global wu .= wu_data
     global wv .= wv_data
 
+    #removing the data from memory
+    w_data = nothing
+    u_data = nothing
+    b_data = nothing
+    U_data = nothing
+    V_data = nothing
+    wu_data = nothing
+    wv_data = nothing
+    GC.gc()
+
     # function calls
-    wprime2 = VKE(w.data, u★)
-    @show size(wprime2)
-    initial_data = wprime2[1, 1, :, 1]
-    x_obs = Observable(initial_data)
-
-    # plotting results
-    n = Observable(1)
-    pt = 1
-    axis_kwargs = (xlabel="y (m)",
-                ylabel="z (m)",
-                aspect = AxisAspect(grid.Lx/grid.Lz),
-                limits = ((0, grid.Lx), (-grid.Lz, 0)))
-    fig = Figure(size = (850, 850))
-
-    # w surface plane slice
-    wxy_title = @lift string("w(x, y, t), at z=-8 m and t = ", prettytime(times[$n]))
-    ax_wxy = Axis(fig[1, 1:2];
-                xlabel = "x (m)",
-                ylabel = "y (m)",
-                aspect = DataAspect(),
-                limits = ((0, grid.Lx), (0, grid.Ly)),
-                title = wxy_title)
-    k = searchsortedfirst(znodes(grid, Face(); with_halos=false), -8)
-    wxyₙ = @lift view(w[$n], :, :, k)
-    wlims = (-0.02, 0.02)
-    hm_wxy = heatmap!(ax_wxy, wxyₙ;
-                    colorrange = wlims,
-                    colormap = :balance)
-    Colorbar(fig[1, 3], hm_wxy; label = "m s⁻¹")
-
-    # w yz plane slice
-    wxz_title = @lift string("w(x, z, t), at x=0 m and t = ", prettytime(times[$n]))
-    ax_wxz = Axis(fig[2, 1:2]; title = wxz_title, axis_kwargs...)
-    wxzₙ = @lift view(w[$n], 1, :, :)
-    hm_wxz = heatmap!(ax_wxz, wxzₙ;
-                    colorrange = wlims,
-                    colormap = :balance)
-
-    Colorbar(fig[2, 3], hm_wxz; label = "m s⁻¹")
-
-    # u yz plane slice
-    uxz_title = @lift string("u(x, z, t), at x=0 m and t = ", prettytime(times[$n]))
-    ax_uxz = Axis(fig[3, 1:2]; title = uxz_title, axis_kwargs...)
-    uₙ = @lift u[$n]
-    uxzₙ = @lift view(u[$n], 1, :, :)
-    ulims = (-0.1, 0.1)
-    ax_uxz = heatmap!(ax_uxz, uxzₙ;
-                    colorrange = ulims,
-                    colormap = :balance)
-
-    Colorbar(fig[3, 3], ax_uxz; label = "m s⁻¹")
-
-    # buoyancy with depth
-    ax_B = Axis(fig[1, 4:5];
-                xlabel = "Buoyancy (m s⁻²)",
-                ylabel = "z (m)",
-                limits = ((minimum(B.data[:, :, :, :]), maximum(B.data[:, :, :, :])), nothing))
-    Bₙ = @lift view(B[$n], 1, 1, :)
-    lines!(ax_B, Bₙ)
-
-    # mean horizontal velocities with depth
-    ax_U = Axis(fig[2, 4:5];
-                xlabel = "Velocities (m s⁻¹)",
-                ylabel = "z (m)",
-                limits = ((minimum(U.data[:, :, :, :]), maximum(U.data[:, :, :, :])), nothing))
-    Uₙ = @lift view(U[$n], 1, 1, :)
-    Vₙ = @lift view(V[$n], 1, 1, :)
-    lines!(ax_U, Uₙ; label = L"\bar{u}")
-    lines!(ax_U, Vₙ; label = L"\bar{v}")
-    axislegend(ax_U; position = :rb)
-
-    # momentum fluxes with depth
-    ax_fluxes = Axis(fig[3, 4:5];
-                    xlabel = "Momentum fluxes (m² s⁻²)",
-                    ylabel = "z (m)",
-                    limits = ((minimum(wu.data[:, :, :, :]), maximum(wu.data[:, :, :, :])), nothing))
-    wuₙ = @lift view(wu[$n], 1, 1, :)
-    wvₙ = @lift view(wv[$n], 1, 1, :)
-    lines!(ax_fluxes, wuₙ; label = L"\overline{wu}")
-    lines!(ax_fluxes, wvₙ; label = L"\overline{wv}")
-    axislegend(ax_fluxes; position = :rb)
-
-    #VKE
-    ax_fluxes = Axis(fig[4, 4:5];
-                    xlabel = L"\overline{w'²} / u★²",
-                    ylabel = "z (m)",
-                    limits = ((0.0, 5.0), nothing))
-    lines!(ax_fluxes, x_obs,  w.grid.z.cᵃᵃᶜ[1:p.Nz+1]; label = L"\overline{w'²} / u★²")
-    axislegend(ax_fluxes; position = :rb)
-
-    fig
-
-    frames = 1:length(times)
-
-    record(fig, "plotting.mp4", frames, framerate=8) do i
-        n[] = i
-        x_obs[] = wprime2[:, i]
-    end 
-end 
-
-function only_plot()
-    grid = RectilinearGrid(size = (p.Nx, p.Ny, p.Nz), extent = (p.Lx, p.Ly, p.Lz), halo = (3, 3, 3))
-    # function calls
+    println("Calculating VKE")
     wprime2 = VKE(w.data, u★)
     @show size(wprime2)
     initial_data = wprime2[1, 1, :, 1]
