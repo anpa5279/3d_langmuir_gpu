6d0fc09220852927af90f8dd0d93d94cfaed50bf
anpa5279 <anpa5279@colorado.edu>
2025-04-25 10:13:50 -0600

    gradient boundar gradient for temperature

--- langmuir_turb.jl
+++ langmuir_turb.jl
@@ -92,7 +92,7 @@ u_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(τx))
 buoyancy = SeawaterBuoyancy(equation_of_state=LinearEquationOfState(thermal_expansion = 2e-4), constant_salinity = 35.0)
 @show buoyancy
 T_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(p.Q / (p.cᴾ * p.ρₒ * p.Lx * p.Ly)),
-                                bottom = GradientBoundaryCondition(p.dTdz))
+                                bottom = GradientBoundaryCondition(0.0))
 #coriolis = FPlane(f=1e-4) # s⁻¹
 
 model = NonhydrostaticModel(; grid, buoyancy, #coriolis,
