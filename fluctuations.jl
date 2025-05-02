0858ace7abcff169c6215ff66ddbd64941be9f06
anpa5279 <anpa5279@colorado.edu>
2025-05-02 11:00:28 -0600

    updating fluctuation function

--- fluctuations.jl
+++ fluctuations.jl
@@ -3,9 +3,8 @@ function fluctuation_xy(a::Field)
     a_avg_xy = Field(Average(a, dims=(1, 2)))  
     @show a_avg_xy
     compute!(a_avg_xy)                         # Evaluate the average on the GPU
-    @show a_avg_xy.data
     # Create an output Field with the same grid
-    a_fluctuation = Field(a.grid)
+    a_fluctuation = Field{Center, Center, Face}(a.grid)
     @show a_fluctuation
     # GPU-friendly broadcasting subtraction
     @. a_fluctuation.data = a.data - a_avg_xy.data
