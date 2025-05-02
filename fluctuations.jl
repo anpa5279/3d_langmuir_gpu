12982b7f9e210fa6bede324c3e32a91125437504
anpa5279 <anpa5279@colorado.edu>
2025-05-02 15:46:14 -0600

    testing

--- fluctuations.jl
+++ fluctuations.jl
@@ -2,7 +2,7 @@ using CUDA
 using Statistics
 function fluctuation_xy(a)
     # Compute horizontal (x, y) average at each z level
-    a_avg_xy = Statistics.mean(a, dims=(1, 2))
+    CUDA.@allowscalar a_avg_xy = Statistics.mean(a, dims=(1, 2))
     @show a_avg_xy
     # Create an output Field with the same grid
     CUDA.@allowscalar a_fluctuation = a .- a_avg_xy
