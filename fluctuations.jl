259c5b09774ecd8f94575b0862025517d7906169
anpa5279 <anpa5279@colorado.edu>
2025-05-02 15:14:11 -0600

    testing

--- fluctuations.jl
+++ fluctuations.jl
@@ -5,7 +5,7 @@ function fluctuation_xy(a::Field)
     compute!(a_avg_xy)                         # Evaluate the average on the GPU
     # Create an output Field with the same grid
     a_fluctuation = Field(a - a_avg_xy)
-    @show a_fluctuation
+    @show a_fluctuation.data
     compute!(a_fluctuation)                     # Evaluate the fluctuation on the GPU
     @show a_fluctuation
     return a_fluctuation
