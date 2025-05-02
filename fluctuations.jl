9f4eaaea0ec430a2edc1832f2ecbd82ea0e9119f
anpa5279 <anpa5279@colorado.edu>
2025-05-02 15:26:15 -0600

    testing

--- fluctuations.jl
+++ fluctuations.jl
@@ -3,7 +3,7 @@ function fluctuation_xy(a)
     a_avg_xy = Satistics.mean(a, dims=(1, 2))
     @show a_avg_xy
     # Create an output Field with the same grid
-    a_fluctuation = a .- a_avg_xy
+    @allowscalr a_fluctuation = a .- a_avg_xy
     @show a_fluctuation
     return a_fluctuation
 end
