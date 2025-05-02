a53247f0aeaab6fd380665b8a15971a00624bf68
anpa5279 <anpa5279@colorado.edu>
2025-05-02 10:41:03 -0600

    updating fluctuation function

--- fluctuations.jl
+++ fluctuations.jl
@@ -1,11 +1,12 @@
 function fluctuation_xy(a::Field)
     # Compute horizontal (x, y) average at each z level
     a_avg_xy = Field(Average(a, dims=(1, 2)))  
+    @show a_avg_xy
     compute!(a_avg_xy)                         # Evaluate the average on the GPU
-
+    @show a_avg_xy.data
     # Create an output Field with the same grid
     a_fluctuation = Field(a.grid)
-
+    @show a_fluctuation
     # GPU-friendly broadcasting subtraction
     @. a_fluctuation.data = a.data - a_avg_xy.data
 
