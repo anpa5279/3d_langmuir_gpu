5edc1d968534e28c7b5eab424e62c40222b879bb
anpa5279 <anpa5279@colorado.edu>
2025-05-02 15:20:06 -0600

    testing

--- fluctuations.jl
+++ fluctuations.jl
@@ -1,12 +1,9 @@
-function fluctuation_xy(a::Field)
+function fluctuation_xy(a)
     # Compute horizontal (x, y) average at each z level
-    a_avg_xy = Field(Average(a, dims=(1, 2)))  
+    a_avg_xy = Satistics.mean(a, dims=(1, 2))
     @show a_avg_xy
-    compute!(a_avg_xy)                         # Evaluate the average on the GPU
     # Create an output Field with the same grid
-    a_fluctuation = Field(a - a_avg_xy)
-    @show a_fluctuation.data
-    compute!(a_fluctuation)                     # Evaluate the fluctuation on the GPU
+    a_fluctuation = a .- a_avg_xy
     @show a_fluctuation
     return a_fluctuation
 end
