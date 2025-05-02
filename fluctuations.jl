6406b9a06896ef53cf8321892e251bb6fffe34a1
anpa5279 <anpa5279@colorado.edu>
2025-05-02 12:08:10 -0600

    updating fluctuation function

--- fluctuations.jl
+++ fluctuations.jl
@@ -1,6 +1,6 @@
 function fluctuation_xy(a::Field)
     # Compute horizontal (x, y) average at each z level
-    a_avg_xy = CUDA.mean(a.data, dims=(1, 2))                     # Evaluate the average on the GPU
+    a_avg_xy = Field{Center, Center, Face}(Average(a, dims=(1, 2)))                     # Evaluate the average on the GPU
     @show a_avg_xy
     # Create an output Field with the same grid
     a_fluctuation = Field{Center, Center, Face}(a.grid)
