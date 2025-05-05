ad9326db73a3010dd46fad4718353097c53bbfc0
anpa5279 <anpa5279@colorado.edu>
2025-05-05 11:21:07 -0600

    ensuring fluct avg is a field

--- fluctuations.jl
+++ fluctuations.jl
@@ -1,7 +1,7 @@
 function fluctuation_xy(a::Field)
     # Compute horizontal (x, y) average at each z level
     @show a
-    a_avg_xy = Average(a, dims=(1, 2)) 
+    a_avg_xy = Field(Average(a, dims=(1, 2))) 
     compute!(a_avg_xy)                         # Evaluate the average on the GPU
     @show a_avg_xy
     @show a_avg_xy.data
