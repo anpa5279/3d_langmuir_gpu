bd7ac39e06aee11e4608180dbc2b86598e78adda
anpa5279 <anpa5279@colorado.edu>
2025-05-05 10:58:33 -0600

    adding printouts

--- fluctuations.jl
+++ fluctuations.jl
@@ -1,5 +1,6 @@
 function fluctuation_xy(a::Field)
     # Compute horizontal (x, y) average at each z level
+    @show a
     a_avg_xy = Average(a, dims=(1, 2)) 
     compute!(a_avg_xy)                         # Evaluate the average on the GPU
     @show a_avg_xy
