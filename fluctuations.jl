48f47bf298cdfff3f25c587f595472f22115ed0e
anpa5279 <anpa5279@colorado.edu>
2025-05-05 09:45:04 -0600

    attempting to get the fluctuation with oceananigans fields

--- fluctuations.jl
+++ fluctuations.jl
@@ -1,11 +1,12 @@
-using CUDA
-using Statistics
-function fluctuation_xy(a)
+function fluctuation_xy(a::Field)
     # Compute horizontal (x, y) average at each z level
-    CUDA.@allowscalar a_avg_xy = Statistics.mean(a, dims=(1, 2))
+    a_avg_xy = Field(Average(a, dims=(1, 2)))  
     @show a_avg_xy
+    compute!(a_avg_xy)                         # Evaluate the average on the GPU
     # Create an output Field with the same grid
-    CUDA.@allowscalar a_fluctuation = a .- a_avg_xy
+    a_fluctuation = Field{Center, Center, Face}(a.grid)
     @show a_fluctuation
+    # GPU-friendly broadcasting subtraction
+    CUDA.@allowscalar @. a_fluctuation.data = a.data .- a_avg_xy.data
     return a_fluctuation
 end
