b25fbcaf795c295910f37d73dfdf18c8c93e352f
anpa5279 <anpa5279@colorado.edu>
2025-05-05 12:04:42 -0600

    trying set

--- fluctuations.jl
+++ fluctuations.jl
@@ -1,10 +1,11 @@
 function fluctuation_xy(a::Field)
     # Create average field (wrapped around lazy Average)
-    a_avg_xy = Field{Nothing, Nothing, Center}(a.grid, Average(a, dims=(1, 2)))
-    compute!(a_avg_xy)  # Now a_avg_xy.data is ready
+    a_avg_xy = Field{Nothing, Nothing, Center}(a.grid)
+    set!(a_avg_xy, Average(a, dims=(1, 2)))  # Now a_avg_xy.data is ready
     @show a_avg_xy
     # Create fluctuation field with same grid and architecture
-    a_fluctuation = Field{Center, Center, Face}(a.grid, a - a_avg_xy)
+    a_fluctuation = Field{Center, Center, Face}(a.grid)
+    set!(a_fluctuation, a .- a_avg_xy)
 
     # GPU-friendly broadcast subtraction
     compute!(a_fluctuation)
