ec3fe677018ed71e85df671d4ac72d16db3e0c7d
anpa5279 <anpa5279@colorado.edu>
2025-05-05 11:30:04 -0600

    figuring out data issue

--- fluctuations.jl
+++ fluctuations.jl
@@ -1,10 +1,10 @@
 function fluctuation_xy(a::Field)
     # Compute horizontal (x, y) average at each z level
     @show a
-    a_avg_xy = Field(Average(a, dims=(1, 2))) 
+    a_avg_xy = Field{Center, Center, Face}(a.grid)
+    a_avg_xy .= Average(a, dims=(1, 2))
     compute!(a_avg_xy)                         # Evaluate the average on the GPU
     @show a_avg_xy
-    @show a_avg_xy.data
     # Create an output Field with the same grid
     a_fluctuation = Field{Center, Center, Face}(a.grid)
     @show a_fluctuation
