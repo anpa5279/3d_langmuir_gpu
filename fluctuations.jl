dbed80de6676872aa66bd901c579b360a6a36e71
anpa5279 <anpa5279@colorado.edu>
2025-05-05 12:37:59 -0600

    figuring out how to use allowscalar

--- fluctuations.jl
+++ fluctuations.jl
@@ -1,10 +1,10 @@
 function fluctuation_xy(a::Field)
     # Create average field (wrapped around lazy Average)
     a_avg_xy = Field(Average(a, dims=(1, 2)))  # Now a_avg_xy.data is ready
-    @show a_avg_xy
+    compute!(a_avg_xy)  # Compute the average field
     # Create fluctuation field with same grid and architecture
     a_fluctuation = Field{Center, Center, Face}(a.grid)
-    set!(a_fluctuation, a .- a_avg_xy)
+    CUDA.allowscalar() a_fluctuation.= a .- a_avg_xy)
 
     # GPU-friendly broadcast subtraction
     compute!(a_fluctuation)
