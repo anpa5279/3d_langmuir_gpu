5c907f4c100b838063569787169b93f5fb83a706
anpa5279 <anpa5279@colorado.edu>
2025-05-09 17:44:17 -0600

    preliminary testing for forcing function

---
+++ smagorinksy_forcing.jl
@@ -0,0 +1,29 @@
+using Oceananigans.Operators: Δxᶜᶜᶜ, Δyᶜᶜᶜ, Δzᶜᶜᶜ, ∂x, ∂y, ∂z
+using Oceananigans.TurbulenceClosures.ΣᵢⱼΣᵢⱼᶜᶜᶜ
+@kernel function smagorinksy_forcing!(i, j, k, grid, clock, model_fields, p)
+    C = p.C
+    grid = w.grid
+    Δyᶜᶜᶜ = grid.Δyᶜᶜᶜ
+    Δxᶜᶜᶜ = grid.Δxᶜᶜᶜ
+    Δzᶜᶜᶜ = grid.Δzᶜᶜᶜ
+    Δx = Δxᶜᶜᶜ(i, j, k, grid)
+    Δy = Δyᶜᶜᶜ(i, j, k, grid)
+    Δz = Δzᶜᶜᶜ(i, j, k, grid)
+    u = model_fields.u
+    v = model_fields.v
+    w = model_fields.w
+    #calcualte the resolved strain rate 0.5*(u_i,j+u_j,i)
+    strain_rate = ΣᵢⱼΣᵢⱼᶜᶜᶜ(i, j, k, grid, u, v, w)
+    #calculating filter width
+    Δ³ = Δx * Δy * Δz
+    Δᶠ = cbrt(Δ³)
+    #calculate the eddy viscosity
+    νₑ = sqrt(2*strain_rate^2)*(C *  Δᶠ)^2
+    #calcaulate subgrid stress
+    τ = -2 * νₑ * strain_rate
+    #calculate the subgrid forcing
+    ∂τ∂x = ∂x(τ)
+    ∂τ∂y= ∂y(τ)
+    ∂τ∂z = ∂z(τ)
+    return @inbounds sqrt(∂τ∂x^2 + ∂τ∂y^2 + ∂τ∂z^2)
+end
