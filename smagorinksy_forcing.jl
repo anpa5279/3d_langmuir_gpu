25f7c4b4bcefdad1fb623e41afb34a740356ab11
anpa5279 <anpa5279@colorado.edu>
2025-05-12 13:51:33 -0600

    adding functions locally

--- smagorinksy_forcing.jl
+++ smagorinksy_forcing.jl
@@ -8,8 +8,6 @@ using KernelAbstractions: @kernel, @index
 using Oceananigans.Operators: Δxᶜᶜᶜ, Δyᶜᶜᶜ, Δzᶜᶜᶜ
 using Oceananigans.AbstractOperations: ∂x, ∂y, ∂z
 
-include("scale_invariant_operators.jl")
-
 @kernel function smagorinksy_forcing!(i, j, k, grid, clock, model_fields, p)
     C = p.C
     grid = w.grid
@@ -23,17 +21,35 @@ include("scale_invariant_operators.jl")
     v = model_fields.v
     w = model_fields.w
     #calcualte the resolved strain rate 0.5*(u_i,j+u_j,i)
-    strain_rate = ΣᵢⱼΣᵢⱼᶜᶜᶜ(i, j, k, grid, u, v, w)
+    strain_rate2 = ΣᵢⱼΣᵢⱼᶜᶜᶜ(i, j, k, grid, u, v, w)
     #calculating filter width
     Δ³ = Δx * Δy * Δz
     Δᶠ = cbrt(Δ³)
     #calculate the eddy viscosity
-    νₑ = sqrt(2*strain_rate^2)*(C *  Δᶠ)^2
+    νₑ = sqrt(2*strain_rate2)*(C *  Δᶠ)^2
     #calcaulate subgrid stress
-    τ = -2 * νₑ * strain_rate
+    τ = -2 * νₑ * strain_rate2
     #calculate the subgrid forcing
     ∂τ∂x = Field(∂x(τ))
     ∂τ∂y= Field(∂y(τ))
     ∂τ∂z = Field(∂z(τ))
     return @inbounds sqrt(∂τ∂x^2 + ∂τ∂y^2 + ∂τ∂z^2)
 end
+
+@inline ΣᵢⱼΣᵢⱼᶜᶜᶜ(i, j, k, grid, u, v, w) =      tr_Σ²(i, j, k, grid, u, v, w) +
+                                            2 * ℑxyᶜᶜᵃ(i, j, k, grid, Σ₁₂², u, v, w) +
+                                            2 * ℑxzᶜᵃᶜ(i, j, k, grid, Σ₁₃², u, v, w) +
+                                            2 * ℑyzᵃᶜᶜ(i, j, k, grid, Σ₂₃², u, v, w)
+
+
+@inline tr_Σ²(ijk...) = Σ₁₁(ijk...)^2 +  Σ₂₂(ijk...)^2 +  Σ₃₃(ijk...)^2
+
+@inline Σ₁₂²(i, j, k, grid, u, v, w) = Σ₁₂²(i, j, k, grid, u, v)
+@inline Σ₁₃²(i, j, k, grid, u, v, w) = Σ₁₃²(i, j, k, grid, u, w)
+@inline Σ₂₃²(i, j, k, grid, u, v, w) = Σ₂₃²(i, j, k, grid, v, w)
+
+@inline Σ₁₁(i, j, k, grid, u) = ∂xᶜᶜᶜ(i, j, k, grid, u)
+@inline Σ₂₂(i, j, k, grid, v) = ∂yᶜᶜᶜ(i, j, k, grid, v)
+@inline Σ₃₃(i, j, k, grid, w) = ∂zᶜᶜᶜ(i, j, k, grid, w)
+
+
