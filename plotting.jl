f2547720e846377bfabbc37d38da573f55b83572
anpa5279 <anpa5279@colorado.edu>
2025-06-18 13:53:06 -0600

    updated plotting to show the differences between the miniapp on fortran

--- plotting.jl
+++ plotting.jl
@@ -1,325 +1,153 @@
 using Pkg
-using OffsetArrays
-using Statistics
-using CairoMakie
+using Plots
 using Printf
 using JLD2
 using Oceananigans
-using Oceananigans.Units: minute, minutes, hours
-using Oceananigans.BuoyancyFormulations: g_Earth
-mutable struct Params
-    Nx::Int         # number of points in each of x direction
-    Ny::Int         # number of points in each of y direction
-    Nz::Int         # number of points in the vertical direction
-    Lx::Float64     # (m) domain horizontal extents
-    Ly::Float64     # (m) domain horizontal extents
-    Lz::Float64     # (m) domain depth 
-    N²::Float64     # s⁻², initial and bottom buoyancy gradient
-    initial_mixed_layer_depth::Float64 # m 
-    Q::Float64      # W m⁻², surface heat flux. cooling is positive
-    cᴾ::Float64     # J kg⁻¹ K⁻¹, specific heat capacity of seawater
-    ρₒ::Float64     # kg m⁻³, average density at the surface of the world ocean
-    dTdz::Float64   # K m⁻¹, temperature gradient
-    T0::Float64     # C, temperature at the surface   
-    β::Float64      # 1/K, thermal expansion coefficient
-    u₁₀::Float64    # (m s⁻¹) wind speed at 10 meters above the ocean
-    La_t::Float64   # Langmuir turbulence number
+using Measures
+
+# opening oceananigans output file
+fld_file="outputs/box_model.jld2"
+
+CO₂_oc = FieldTimeSeries(fld_file, "CO₂")
+CO₃_oc = FieldTimeSeries(fld_file, "CO₃")
+HCO₃_oc = FieldTimeSeries(fld_file, "HCO₃")
+H_oc = FieldTimeSeries(fld_file, "H")
+OH_oc = FieldTimeSeries(fld_file, "OH")
+BOH₃_oc = FieldTimeSeries(fld_file, "BOH₃")
+BOH₄_oc = FieldTimeSeries(fld_file, "BOH₄")
+t = CO₂_oc.times
+dt = Float64[t.step][1]
+
+CO₂_oc = vec(CO₂_oc.data)
+CO₃_oc = vec(CO₃_oc.data)
+HCO₃_oc = vec(HCO₃_oc.data)
+H_oc = vec(H_oc.data)
+OH_oc = vec(OH_oc.data)
+BOH₃_oc = vec(BOH₃_oc.data)
+BOH₄_oc = vec(BOH₄_oc.data)
+
+# opening fortran output file
+fortran_file = "outputs/cc.hst"
+f = open(fortran_file)
+
+CO₂_f = Float64[]
+CO₃_f = Float64[]
+HCO₃_f = Float64[]
+OH_f = Float64[]
+BOH₃_f = Float64[]
+BOH₄_f = Float64[]
+
+CO₂_d = Float64[]
+CO₃_d = Float64[]
+HCO₃_d = Float64[]
+OH_d = Float64[]
+BOH₃_d = Float64[]
+BOH₄_d = Float64[]
+
+line_count = 0              
+
+for lines in readlines(f)
+     # Split the line into string tokens and parse to Float64
+     values = parse.(Float64, split(lines))
+     #@show round(values[1]*24*60*60)
+     if rem(round(values[1]*24*60*60, digits = 3), dt) == 0.0 || line_count == 0
+          # increment line_count
+          global line_count += 1 
+          # Store in respective arrays (assuming correct ordering in file)
+          push!(CO₂_f,   values[2]/(1e6))
+          push!(HCO₃_f,  values[3]/(1e6))
+          push!(CO₃_f,   values[4]/(1e6))
+          push!(BOH₃_f,  values[5]/(1e6))
+          push!(BOH₄_f,  values[6]/(1e6))
+          push!(OH_f,    values[7]/(1e6))
+          push!(CO₂_d,  (CO₂_oc[line_count] - CO₂_f[line_count]) / CO₂_oc[line_count] * 100)
+          push!(CO₃_d,  (CO₃_oc[line_count] - CO₃_f[line_count]) / CO₃_oc[line_count] * 100)
+          push!(HCO₃_d, (HCO₃_oc[line_count] - HCO₃_f[line_count]) / HCO₃_oc[line_count] * 100)
+          push!(OH_d,   (OH_oc[line_count] - OH_f[line_count]) / OH_oc[line_count] * 100) 
+          push!(BOH₃_d, (BOH₃_oc[line_count] - BOH₃_f[line_count]) / BOH₃_oc[line_count] * 100)
+          push!(BOH₄_d, (BOH₄_oc[line_count] - BOH₄_f[line_count]) / BOH₄_oc[line_count] * 100)
+     end 
+
 end
 
-#defaults, these can be changed directly below 128, 128, 160, 320.0, 320.0, 96.0
-p = Params(128, 128, 160, 320.0, 320.0, 96.0, 5.3e-9, 33.0, 0.0, 4200.0, 1000.0, 0.01, 17.0, 2.0e-4, 5.75, 0.3)
-grid = RectilinearGrid(size = (p.Nx, p.Ny, p.Nz), extent = (p.Lx, p.Ly, p.Lz), halo = (3, 3, 3))
+# plotting results
 
-include("stokes.jl")
+# CO₂
+CO₂p = plot(t, [CO₂_oc CO₂_f],
+     xlabel = "t (s)", ylabel = "CO₂ (mol m⁻³)",
+     title = "CO₂")
+# CO₃
+CO₃p = plot(t, [CO₃_oc CO₃_f],
+     xlabel = "t (s)", ylabel = "CO₃ (mol m⁻³)",
+     title = "CO₃")
+# HCO₃
+HCO₃p = plot(t, [HCO₃_oc HCO₃_f],
+     xlabel = "t (s)", ylabel = "HCO₃ (mol m⁻³)",
+     title = "HCO₃")
+# OH
+OHp = plot(t, [OH_oc OH_f],
+     xlabel = "t (s)", ylabel = "OH (mol m⁻³)",
+     title = "OH")
+# BOH₃
+BOH₃p = plot(t, [BOH₃_oc BOH₃_f],
+     xlabel = "t (s)", ylabel = "BOH₃ (mol m⁻³)",
+     title = "BOH₃")
+# BOH₄
+BOH₄p = plot(t, [BOH₄_oc BOH₄_f],
+     xlabel = "t (s)", ylabel = "BOH₄ (mol m⁻³)",
+     title = "BOH₄")
+# layout 
+gr()
+pf = plot(CO₂p, CO₃p, HCO₃p, OHp, BOH₃p, BOH₄p, layout = (3, 2), label = ["Oceananigans" "Fortran Miniapp"], legend = :outertop,         # << shared legend at the top
+           legendcolumns = 2,
+           size = (1600, 1200),
+           left_margin = 30mm,
+           bottom_margin = 10mm,
+           plot_margin = 5mm, 
+           titlefont = font(12), 
+           tickfont = font(9), 
+           labelfont = font(10), 
+           xlabelfont = font(10), 
+           ylabelfont = font(10), 
+           titlefontsize = 12)
+png(pf, "outputs/box_model-rkc77.png")
 
-function make_field_center(data, times)
-    field = FieldTimeSeries{Center, Center, Center}(grid, times)
-    field.data .= data
-    return field
-end
-
-function make_field_face(data, times)
-    # Create a FieldTimeSeries object from the data
-    field = FieldTimeSeries{Center, Center, Face}(grid, times)
-    field.data .= data
-    return field
-end
-
-function video()
-    Nranks = 4
-    fld_file="outputs/langmuir_turbulence_fields_0.jld2"
-    f = jldopen(fld_file)
-
-    #time and IC data 
-    u★ = f["IC"]["friction_velocity"]
-    #u_stokes = f["IC"]["stokes_velocity"]
-    #u₁₀ = f["IC"]["wind_speed"]
-    t_save = collect(keys(f["timeseries"]["t"]))
-    close(f)
-
-    Nt = length(t_save)
-
-    times = Array{Float64}(undef, Int(Nt/2))
-    w_data = OffsetArray{Float64}(undef, -grid.Hx+1 : p.Nx+grid.Hx,
-                                        -grid.Hy+1 : p.Ny+grid.Hy,
-                                        -grid.Hz+1 : p.Nz+1+grid.Hz,
-                                        1 : Int(Nt/2))
-    u_data = OffsetArray{Float64}(undef, -grid.Hx+1 : p.Nx+grid.Hx,
-                                        -grid.Hy+1 : p.Ny+grid.Hy,
-                                        -grid.Hz+1 : p.Nz+grid.Hz,
-                                        1 : Int(Nt/2))
-    #b_data = OffsetArray{Float64}(undef, -grid.Hx+1 : p.Nx+grid.Hx,
-    #                                    -grid.Hy+1 : p.Ny+grid.Hy,
-    #                                    -grid.Hz+1 : p.Nz+grid.Hz,
-    #                                    1 : Nt)
-    T_data =  Array{Float64}(undef, (1, 1, p.Nz, Int(Nt/2)))
-    U_data = Array{Float64}(undef, (1, 1, p.Nz, Int(Nt/2)))
-    V_data = Array{Float64}(undef, (1, 1, p.Nz, Int(Nt/2)))
-    wu_data = Array{Float64}(undef, (1, 1, p.Nz + 1, Int(Nt/2)))
-    wv_data =   Array{Float64}(undef, (1, 1, p.Nz + 1, Int(Nt/2)))
-    T_data .= 0
-    U_data .= 0
-    V_data .= 0
-    wu_data .= 0
-    wv_data .= 0
-
-    for i in 0:Nranks-1
-        println("Loading rank $i")
-
-        fld_file="outputs/langmuir_turbulence_fields_$(i).jld2"
-        averages_file="outputs/langmuir_turbulence_averages_$(i).jld2"
-
-        f = jldopen(fld_file)
-        T_temp = FieldTimeSeries(averages_file, "T_avg")
-        U_temp = FieldTimeSeries(averages_file, "U")
-        V_temp = FieldTimeSeries(averages_file, "V")
-        W_temp = FieldTimeSeries(averages_file, "W")
-        wu_temp = FieldTimeSeries(averages_file, "wu")
-        wv_temp = FieldTimeSeries(averages_file, "wv")
-        if i == 0 #first rank
-            shift = 0
-            Nr = Int(p.Nx / Nranks + grid.Hx)
-            xrange = 1 : Nr #last rank
-        elseif i == Nranks - 1
-            shift  = -2 * grid.Hx
-            Nr = Int(p.Nx / Nranks + grid.Hx)
-            xrange = grid.Hx + 1 : grid.Hx + Nr
-        else #middle ranks
-            shift = grid.Hx
-            Nr = Int(p.Nx / Nranks)
-            xrange = grid.Hx + 1 : grid.Hx + Nr
-        end 
-        nn = 1 + shift + i * Nr - grid.Hx
-        w_all = [f["timeseries"]["w"][t][xrange, :, :] for t in t_save]
-        u_yplane = [f["timeseries"]["u"][t][1, :, :] for t in t_save]
-        #b_all = [f["timeseries"]["b"][t][xrange, :, :] for t in t_save]
-        j = 1
-        for k in 1:2:Nt
-            @show k, j
-            times[j] = f["timeseries"]["t"][t_save[j]]
-            local w = w_all[k]
-            local u = u_yplane[k]
-            #local b = b_all[k]
-            w_data[nn:nn + Nr - 1, :, :, j] = w
-            u_data[1, :, :, j] = u
-            #b[nn:nn + Nr - 1, :, :, k] = b
-            j += 1
-            #removing the data from memory
-            w = nothing
-            u = nothing
-            #b = nothing
-            GC.gc()
-        end
-        j = nothing
-        GC.gc()
-        T_data .= T_data .+ T_temp.data[:, :, 1:p.Nz, 1:Int(Nt/2)]
-        U_data .= U_data .+ U_temp.data[:, :, 1:p.Nz, 1:Int(Nt/2)]
-        V_data .= V_data .+ V_temp.data[:, :, 1:p.Nz, 1:Int(Nt/2)]
-        wu_data .= wu_data .+ wu_temp.data[:, :, 1:p.Nz + 1, 1:Int(Nt/2)]
-        wv_data .= wv_data .+ wv_temp.data[:, :, 1:p.Nz + 1, 1:Int(Nt/2)]
-        #removing the data from memory
-        w_all = nothing
-        u_yplane = nothing
-        b_all = nothing
-        xrange = nothing
-        Nr = nothing 
-        shift = nothing
-        T_temp = nothing
-        U_temp = nothing
-        V_temp = nothing
-        wu_temp = nothing
-        wv_temp = nothing
-        GC.gc() 
-        close(f)
-    end
-    t_save = nothing
-    GC.gc()
-
-    #averaging
-    println("Averaging data")
-    T_data = T_data ./ Nranks
-    U_data = U_data ./ Nranks
-    V_data = V_data ./ Nranks
-    wu_data = wu_data ./ Nranks
-    wv_data = wv_data ./ Nranks
-
-    #putting everything back into FieldTimeSeries
-    println("Putting data into FieldTimeSeries")
-    w  = make_field_face(w_data, times)
-    w_data = nothing
-    GC.gc()
-    u  = make_field_center(u_data, times)
-    u_data = nothing
-    GC.gc()
-    #b = make_field_center(b_data, times)
-    #b_data = nothing
-    #GC.gc()
-    B = FieldTimeSeries{Center, Center, Center}(grid, times)
-    U = FieldTimeSeries{Center, Center, Center}(grid, times)
-    V = FieldTimeSeries{Center, Center, Center}(grid, times)
-    wu = FieldTimeSeries{Center, Center, Face}(grid, times)
-    wv = FieldTimeSeries{Center, Center, Face}(grid, times)
-    B .= g_Earth * p.β * (T_data .- p.T0)
-    T_data = nothing
-    GC.gc()
-    U .= U_data
-    U_data = nothing
-    GC.gc()
-    V .= V_data
-    V_data = nothing
-    GC.gc()
-    wu .= wu_data
-    wu_data = nothing
-    GC.gc()
-    wv .= wv_data
-    wv_data = nothing
-    GC.gc()
-    # function calls
-    println("Calculating VKE")
-    w_fluct = fluctuations(w.data)
-    w_fluct2 = w_fluct.^2
-    wprime2 = Statistics.mean(a_prime2, dims=(1, 2)) / (u_f^2)
-    initial_data = wprime2[-2, -2, :, 1] #negative indices because of the halo
-    wprime2_obs = Observable(initial_data)
-
-    # plotting results
-    n = Observable(1)
-    pt = 1
-    axis_kwargs = (xlabel="y (m)",
-                ylabel="z (m)",
-                aspect = AxisAspect(grid.Lx/grid.Lz),
-                limits = ((0, grid.Lx), (-grid.Lz, 0)))
-    fig = Figure(size = (850, 850))
-
-    # w surface plane slice
-    wxy_title = @lift string("w(x, y, t), at z=-8 m and t = ", prettytime(times[$n]))
-    ax_wxy = Axis(fig[1, 1:2];
-                xlabel = "x (m)",
-                ylabel = "y (m)",
-                aspect = DataAspect(),
-                limits = ((0, grid.Lx), (0, grid.Ly)),
-                title = wxy_title)
-    k = searchsortedfirst(znodes(grid, Face(); with_halos=false), -8)
-    wxyₙ = @lift view(w[$n], :, :, k)
-    wlims = (-0.02, 0.02)
-    hm_wxy = heatmap!(ax_wxy, wxyₙ;
-                    colorrange = wlims,
-                    colormap = :balance)
-    Colorbar(fig[1, 3], hm_wxy; label = "m s⁻¹")
-
-    # w yz plane slice
-    wxz_title = @lift string("w(x, z, t), at x=0 m and t = ", prettytime(times[$n]))
-    ax_wxz = Axis(fig[2, 1:2]; title = wxz_title, axis_kwargs...)
-    wxzₙ = @lift view(w[$n], 1, :, :)
-    hm_wxz = heatmap!(ax_wxz, wxzₙ;
-                    colorrange = wlims,
-                    colormap = :balance)
-
-    Colorbar(fig[2, 3], hm_wxz; label = "m s⁻¹")
-
-    # u xz plane slice
-    uxz_title = @lift string("u(x, z, t), at x=0 m and t = ", prettytime(times[$n]))
-    ax_uxz = Axis(fig[3, 1:2]; title = uxz_title, axis_kwargs...)
-    uₙ = @lift u[$n]
-    uxzₙ = @lift view(u[$n], 1, :, :)
-    ulims = (-0.1, 0.1)
-    ax_uxz = heatmap!(ax_uxz, uxzₙ;
-                    colorrange = ulims,
-                    colormap = :balance)
-
-    Colorbar(fig[3, 3], ax_uxz; label = "m s⁻¹")
-
-    # buoyancy with depth
-    ax_B = Axis(fig[1, 4:5];
-                xlabel = "Buoyancy (m s⁻²)",
-                ylabel = "z (m)",
-                limits = ((minimum(B.data[:, :, :, :]), maximum(B.data[:, :, :, :])), nothing))
-    Bₙ = @lift view(B[$n], 1, 1, :)
-    lines!(ax_B, Bₙ)
-
-    # mean horizontal velocities with depth
-    ax_U = Axis(fig[2, 4:5];
-                xlabel = "Velocities (m s⁻¹)",
-                ylabel = "z (m)",
-                limits = ((minimum(U.data[:, :, :, :]), maximum(U.data[:, :, :, :])), nothing))
-    Uₙ = @lift view(U[$n], 1, 1, :)
-    Vₙ = @lift view(V[$n], 1, 1, :)
-    lines!(ax_U, Uₙ; label = L"\bar{u}")
-    lines!(ax_U, Vₙ; label = L"\bar{v}")
-    axislegend(ax_U; position = :rb)
-
-    # momentum fluxes with depth
-    ax_fluxes = Axis(fig[3, 4:5];
-                    xlabel = "Momentum fluxes (m² s⁻²)",
-                    ylabel = "z (m)",
-                    limits = ((minimum(wu.data[:, :, :, :]), maximum(wu.data[:, :, :, :])), nothing))
-    wuₙ = @lift view(wu[$n], 1, 1, :)
-    wvₙ = @lift view(wv[$n], 1, 1, :)
-    lines!(ax_fluxes, wuₙ; label = L"\overline{wu}")
-    lines!(ax_fluxes, wvₙ; label = L"\overline{wv}")
-    axislegend(ax_fluxes; position = :rb)
-
-    #VKE
-    ax_VKE = Axis(fig[4, 4:5];
-                    xlabel = L"\overline{w'²} / u★²",
-                    ylabel = "z (m)",
-                    limits = ((0.0, 5.0), nothing))
-    lines!(ax_VKE, wprime2_obs, grid.z.cᵃᵃᶠ; label = L"\overline{w'²} / u★²")
-    axislegend(ax_VKE; position = :rb)
-
-    fig
-
-    frames = 1:length(times)
-
-    record(fig, "langmuir_turbulence_temp_buoy.mp4", frames, framerate=8) do i
-        n[] = i
-        wprime2_obs[] = wprime2[-2, -2, :, i]
-    end
-end
-
-function stokes_plot()
-    Nranks = 4
-    dudz = Array{Float64}(undef, (1, 1, p.Nz))
-    for i in 0:Nranks-1
-        println("Loading rank $i")
-
-        fld_file="outputs/langmuir_turbulence_fields_0.jld2"
-
-        f = jldopen(fld_file)
-        keys(f)
-        dudz = f["stokes_drift_field"]
-        GC.gc() 
-        close(f)
-    end
-    z = collect(-p.Lz + grid.z.Δᵃᵃᶜ/2 : grid.z.Δᵃᵃᶜ : -grid.z.Δᵃᵃᶜ/2)
-    n = Observable(1)
-    fig 
-    save("oceananigans_stokes_drift.png", fig)
-    #plotting velocity gradient profile
-    fig2 = Figure()
-    ax2 = Axis(fig2[1, 1], xlabel = "duˢ/dz [1/s]", ylabel = "z [m]", title = "Stokes drift gradient")
-    lines!(ax2, dudz, label = "Oceananigans Stokes drift gradient")
-    fig2
-    axislegend(ax2; position = :rb)
-    save("oceananigans_stokes_drift_gradient.png", fig2)
-end 
+#plotting percent differences
+#CO₂
+CO₂p = plot(t, CO₂_d,
+     xlabel = "t (s)", ylabel = "CO₂ %",
+     title = "CO₂")
+# CO₃
+CO₃p = plot(t, CO₃_d,
+     xlabel = "t (s)", ylabel = "CO₃ %",
+     title = "CO₃")
+# HCO₃
+HCO₃p = plot(t, HCO₃_d,
+     xlabel = "t (s)", ylabel = "HCO₃ %",
+     title = "HCO₃")
+# OH
+OHp = plot(t, OH_d,
+     xlabel = "t (s)", ylabel = "OH %",
+     title = "OH")
+# BOH₃
+BOH₃p = plot(t, BOH₃_d,
+     xlabel = "t (s)", ylabel = "BOH₃ %",
+     title = "BOH₃")
+# BOH₄
+BOH₄p = plot(t, BOH₄_d,
+     xlabel = "t (s)", ylabel = "BOH₄ %",
+     title = "BOH₄")
+# layout 
+gr()
+pf = plot(CO₂p, CO₃p, HCO₃p, OHp, BOH₃p, BOH₄p, layout = (3, 2), legend = false,         # << shared legend at the top
+           size = (1600, 1200),
+           left_margin = 30mm,
+           bottom_margin = 10mm,
+           plot_margin = 5mm, 
+           titlefont = font(12), 
+           tickfont = font(9), 
+           labelfont = font(10), 
+           xlabelfont = font(10), 
+           ylabelfont = font(10), 
+           titlefontsize = 12)
+png(pf, "outputs/percent-difference.png")
