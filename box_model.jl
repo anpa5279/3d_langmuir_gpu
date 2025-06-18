5b463b7fd3398ddc30f217100141dfb7e29cce36
anpa5279 <anpa5279@colorado.edu>
2025-06-18 14:48:25 -0600

    update output writer. idk why it is different on my computer

--- box_model.jl
+++ box_model.jl
@@ -35,7 +35,7 @@ biogeochemistry = CarbonateChemistry(; grid)
 
 model = BoxModel(; biogeochemistry, clock)
 
-set!(model, BOH₃ = 2.97e-4, BOH₄ = 1.19e-4, CO₂ = 20.0, CO₃ = 3.15e-4, HCO₃ = 20.0, OH = 9.6e-6, T=25, S = 35)#BOH₃ = 2.97e-4, BOH₄ = 1.19e-4, CO₂ = 7.57e-6, CO₃ = 3.15e-4, H = 6.31e-9, HCO₃ = 1.67e-3, OH = 9.6e-6, T=25, S = 35)
+set!(model, BOH₃ = 2.97e-4, BOH₄ = 1.19e-4, CO₂ = 20.0, CO₃ = 3.15e-4, HCO₃ = 20.0, OH = 9.6e-6, T=25, S = 35)#BOH₃ = 2.97e-4, BOH₄ = 1.19e-4, CO₂ = 7.57e-6, CO₃ = 3.15e-4, HCO₃ = 1.67e-3, OH = 9.6e-6, T=25, S = 35)
 
 simulation = Simulation(model, Δt=1e-7, stop_time = 60seconds) #stop_time = 96hours,
 @show simulation
@@ -49,7 +49,7 @@ CO₃ = model.fields.CO₃
 HCO₃ = model.fields.HCO₃
 OH = model.fields.OH
 
-simulation.output_writers[:fields] = JLD2Writer(model, (; BOH₃, BOH₄, CO₂, CO₃, HCO₃, OH),
+simulation.output_writers[:fields] = JLD2OutputWriter(model, (; BOH₃, BOH₄, CO₂, CO₃, HCO₃, OH),
                                                       schedule = TimeInterval(output_interval),
                                                       filename = "outputs/box_model.jld2", #$(rank)
                                                       overwrite_existing = true)
