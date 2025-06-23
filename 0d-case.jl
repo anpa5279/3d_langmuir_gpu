5109d17bf626f3fe69ea4815da5e2a3bcc06bda2
anpa5279 <anpa5279@colorado.edu>
2025-06-23 09:42:04 -0600

    attempting to convert from mol/kg to umol/kg

--- 0d-case.jl
+++ 0d-case.jl
@@ -11,13 +11,14 @@ using .CC #: CarbonateChemistry #local module
 grid = RectilinearGrid(size=(1, 1, 1), extent=(1.0, 1.0, 1.0))
 bgc = CarbonateChemistry(; grid, scale_negatives = true)#$, 
 
-CO₂ = 7.57e-1
-HCO₃ = 1.67e-3
-CO₃ = 3.15e-4
-BOH₃ = 2.97e-4
-BOH₄ = 1.19e-4
-H = 6.31e-9
-OH = 9.6e-6
+#converting mol/kg to umol/kg
+CO₂ = 7.57e-1 * 1e6 
+HCO₃ = 1.67e-3 * 1e6 
+CO₃ = 3.15e-4 * 1e6 
+BOH₃ = 2.97e-4 * 1e6 
+BOH₄ = 1.19e-4 * 1e6 
+#H = 6.31e-9
+OH = 9.6e-6 * 1e6 
 
 min = 60.0 # seconds per minute
 hr = 60.0 * min # minutes per hour
@@ -28,35 +29,33 @@ function boxmodel_ode!(du, u, p, t)
     CO₂ = u[1]
     HCO₃ = u[2]
     CO₃ = u[3]
-    H = u[4]
-    OH = u[5]
-    BOH₃ = u[6]
-    BOH₄ = u[7]
+    H = 0.0
+    OH = u[4]
+    BOH₃ = u[5]
+    BOH₄ = u[6]
     T = 25
     S = 35
-    du[1] =  bgc(Val(:CO₂),  0, 0, 0, t, CO₂, HCO₃, CO₃, H, OH, BOH₃, BOH₄, T, S)
-    du[2] =  bgc(Val(:HCO₃), 0, 0, 0, t, CO₂, HCO₃, CO₃, H, OH, BOH₃, BOH₄, T, S)
-    du[3] =  bgc(Val(:CO₃),  0, 0, 0, t, CO₂, HCO₃, CO₃, H, OH, BOH₃, BOH₄, T, S)
-    du[4] =  bgc(Val(:H),    0, 0, 0, t, CO₂, HCO₃, CO₃, H, OH, BOH₃, BOH₄, T, S)
-    du[5] =  bgc(Val(:OH),   0, 0, 0, t, CO₂, HCO₃, CO₃, H, OH, BOH₃, BOH₄, T, S)
-    du[6] =  bgc(Val(:BOH₃), 0, 0, 0, t, CO₂, HCO₃, CO₃, H, OH, BOH₃, BOH₄, T, S)
-    du[7] = -du[1]
+    du[1] =  bgc(Val(:CO₂),  0, 0, 0, t, CO₂, HCO₃, CO₃,  OH, BOH₃, BOH₄, T , S, H)
+    du[2] =  bgc(Val(:HCO₃), 0, 0, 0, t, CO₂, HCO₃, CO₃,  OH, BOH₃, BOH₄, T , S, H)
+    du[3] =  bgc(Val(:CO₃),  0, 0, 0, t, CO₂, HCO₃, CO₃,  OH, BOH₃, BOH₄, T , S, H)
+    du[4] =  bgc(Val(:OH),   0, 0, 0, t, CO₂, HCO₃, CO₃,  OH, BOH₃, BOH₄, T , S, H)
+    du[5] =  bgc(Val(:BOH₃), 0, 0, 0, t, CO₂, HCO₃, CO₃,  OH, BOH₃, BOH₄, T , S, H)
+    du[6] = -du[1]
 end
 
 
 function clip_negatives!(integrator)
     @. integrator.u = max(integrator.u, 1e-20)
 end
-
 condition(u, t, integrator) = any(<(0), u)
 cb = DiscreteCallback(condition, clip_negatives!)
 
 t_final = 60 # final time, in seconds
 dt_out = 0.05 # output rate, not solver timestep size, in seconds
-c_0 = [CO₂, HCO₃, CO₃, H, OH, BOH₃, BOH₄]
+c_0 = [CO₂, HCO₃, CO₃, OH, BOH₃, BOH₄]
 tspan = (0.0, t_final)
 prob = ODEProblem(boxmodel_ode!, c_0, tspan)
-sol = solve(prob, Rosenbrock23(), reltol = 1e-10, abstol = 1e-12, saveat = dt_out)#, maxiters = 10_000_000, callback = cb) #KenCarp47(linsolve = KrylovJL_GMRES())
+sol = solve(prob, Rosenbrock23(), reltol = 1e-10, abstol = 1e-12, saveat = dt_out)#, maxiters = 10_000_000, callback = cb) #KenCarp47(linsolve = KrylovJL_GMRES()), solve(prob, alg_hints = [:stiff], reltol = 1e-10, abstol = 1e-15, saveat = dt_out)
 
 @save "outputs/0d-case.jld2" t=sol.t u=sol.u
 
--- cc.jl
+++ cc.jl
@@ -51,11 +51,11 @@ end
 function CarbonateChemistry(; grid::AbstractGrid{FT},
                             #Zeebe and Wolf Gladrow 2001
                             A1::FT = 4.70e7, # kg/mol/s
-                            E1::FT = 1000 * 23.2, # J/mol
+                            E1::FT = 23.2, # kJ/mol
                             A7::FT = 4.58e10, # kg/mol/s
-                            E7::FT = 1000 * 20.8, # J/mol
+                            E7::FT = 20.8, # kJ/mol
                             A8::FT = 3.05e10, # kg/mol/s
-                            E8::FT = 1000 * 20.8, # J/mol
+                            E8::FT = 20.8, # kJ/mol
                             #Dickson and Goyet 1994
                             alpha3::FT = 5e10, # kg/mol/s
                             alpha4::FT = 6.0e9, # kg/mol/s
@@ -89,45 +89,45 @@ function CarbonateChemistry(; grid::AbstractGrid{FT},
                            modifiers)
 end
 
-required_biogeochemical_tracers(::CarbonateChemistry) = (:CO₂, :HCO₃, :CO₃, :H, :OH, :BOH₃, :BOH₄, :T, :S)
+required_biogeochemical_tracers(::CarbonateChemistry) = (:CO₂, :HCO₃, :CO₃, :OH, :BOH₃, :BOH₄, :T, :S)
 required_biogeochemical_auxiliary_fields(::CarbonateChemistry) = ()#(:H,)
 
-const R = 8.31446261815324 # kg⋅m²⋅s⁻²⋅K⁻1⋅mol⁻1
+const R = 0.00831446261815324 # kJ⋅K⁻1⋅mol⁻1
 
 #Dickson and Goyet 1994, who references Roy et al. 1993,  Dickson 1990, and Millero 1994
 @inline K_1(T, S) = exp(-2307.1266 / (T + 273.15) + 2.83655 - 1.5529413 * log(T + 273.15) +
             (-4.0484 / (T + 273.15) - 0.20760841) * sqrt(S) + 0.08468345 * S -
-            0.00654208 * S^1.5 + log(1 - 0.001005 * S))
+            0.00654208 * S^1.5 + log(1 - 0.001005 * S)) * 1.0e6
 
 @inline K_2(T, S) = exp(-3351.6106 / (T + 273.15) - 9.226508 - 0.2005743 * log((T + 273.15)) +
             (-23.9722 / (T + 273.15) - 0.106901773)* sqrt(S) + 0.1130822 * S -
-            0.00846934 * S^1.5 + log(1 - 0.001005 * S))
+            0.00846934 * S^1.5 + log(1 - 0.001005 * S)) * 1.0e6
 
 @inline K_w(T, S) = exp(-13847.26 / (T + 273.15) + 148.9652 - 23.6521 * log((T + 273.15)) +
-            (118.67 / (T + 273.15) - 5.977 + 1.0495 * log((T + 273.15))) * sqrt(S) - 0.01615 * S)
+            (118.67 / (T + 273.15) - 5.977 + 1.0495 * log((T + 273.15))) * sqrt(S) - 0.01615 * S)  * 1.0e6 * 1.0e6
 
 @inline K_b(T, S) = exp((-8966.90 - 2890.53 * sqrt(S) - 77.942 * S + 1.728 * S^1.5 - 
             0.0996 * S^2) / (T + 273.15) + (148.0248 + 137.1942 * sqrt(S) + 
             1.62142 * S) + (-24.4344 - 25.085 * sqrt(S) -0.2474 * S) * log((T + 273.15))
-            + 0.053105 * sqrt(S) * (T + 273.15))
+            + 0.053105 * sqrt(S) * (T + 273.15)) * 1.0e6
 
 #Zeebe and Wolf Gladrow 2001
-@inline alpha1(T) = exp(1246.98 - 6.19e4 / (T + 273.15) - 183.0 * log(T + 273.15)) # kg/mol/s
+@inline alpha1(T) = exp(1246.98 - 6.19e4 / (T + 273.15) - 183.0 * log(T + 273.15)) / 1e6  # kg/umol/s
 @inline beta1(alpha1, K1) = alpha1/ K1 # 1/s
-@inline alpha2(A1, E1, T) = A1 * exp(-E1 / (R * (T + 273.15))) # kg/mol/s
+@inline alpha2(A1, E1, T) = A1 * exp(-E1 / (R * (T + 273.15))) / 1e6 # kg/umol/s
 @inline beta2(alpha2, Kw, K1) = alpha2 * Kw/ K1# 1/s
 @inline beta3(alpha3, K2) = alpha3 * K2 # 1/s
 @inline beta4(alpha4, Kw, K2) = alpha4 * Kw/ K2 # 1/s
-@inline beta5(alpha5, Kw) = alpha5 / Kw # kg/mol/s
-@inline alpha6(A7, E8, T) = A7 * exp(-E8 / (R * (T + 273.15))) # kg/mol/s
+@inline beta5(alpha5, Kw) = alpha5 / Kw * 1e6 # umol/kg/s
+@inline alpha6(A7, E8, T) = A7 * exp(-E8 / (R * (T + 273.15))) / 1e6 # kg/umol/s
 @inline beta6(alpha6, Kw, Kb) =  alpha6* Kw/ Kb # 1/s
-@inline alpha7(A8, E8, T) = A8 * exp(-E8 / (R * (T + 273.15))) # kg/mol/s
-@inline beta7(alpha7, K2, Kb) = alpha7* K2/ Kb # kg/mol/s
+@inline alpha7(A8, E8, T) = A8 * exp(-E8 / (R * (T + 273.15))) / 1e6 # kg/umol/s
+@inline beta7(alpha7, K2, Kb) = alpha7* K2/ Kb / 1e6 # kg/umol/s
 
 #QSS approximation
 @inline H_qss(alpha1, beta1, alpha3, beta3, alpha5, beta5, c1, c2, c3, c5) = (alpha1*c1 + beta3*c2 + alpha5)/(beta1*c2 + alpha3*c3 + beta5*c5)
 #updating tracers 
-@inline function (bgc::CarbonateChemistry)(::Val{:CO₂}, x, y, z, t, CO₂, HCO₃, CO₃, H, OH, BOH₃, BOH₄, T, S)
+@inline function (bgc::CarbonateChemistry)(::Val{:CO₂}, x, y, z, t, CO₂, HCO₃, CO₃, OH, BOH₃, BOH₄, T, S) #, H)
     K1 = K_1(T, S)
     K2 = K_2(T, S)
     Kw = K_w(T, S)
@@ -145,10 +145,10 @@ const R = 8.31446261815324 # kg⋅m²⋅s⁻²⋅K⁻1⋅mol⁻1
     #println("a1 = ", a1, " b1 = ", b1, " a2 = ", a2, " b2 = ", b2)
     if isnan(CO₂) error("CO₂ concentration is NaN") end
     dcdt = - (a1 + a2 * OH) * CO₂ + (b1 * H + b2) * HCO₃
-    return dcdt
+    return dcdt # converting to micromol/kg rate
 end
 
-@inline function (bgc::CarbonateChemistry)(::Val{:HCO₃}, x, y, z, t, CO₂, HCO₃, CO₃, H, OH, BOH₃, BOH₄, T, S)
+@inline function (bgc::CarbonateChemistry)(::Val{:HCO₃}, x, y, z, t, CO₂, HCO₃, CO₃, OH, BOH₃, BOH₄, T, S) #, H)
     
     K1 = K_1(T, S)
     K2 = K_2(T, S)
@@ -171,10 +171,10 @@ end
     H = H_qss(a1, b1, a3, b3, a5, b5, CO₂, HCO₃, CO₃, OH)
     if isnan(HCO₃) error("HCO₃ concentration is NaN") end
     dcdt = (a1 + a2 * OH) * CO₂ - (b1 * H + b2 + b3 + a4 * OH + b7 * BOH₄) * HCO₃ + (a3 * H + b4 + a7 * BOH₃) * CO₃
-    return dcdt
+    return dcdt # converting to micromol/kg rate
 end
 
-@inline function (bgc::CarbonateChemistry)(::Val{:CO₃}, x, y, z, t, CO₂, HCO₃, CO₃, H, OH, BOH₃, BOH₄, T, S)
+@inline function (bgc::CarbonateChemistry)(::Val{:CO₃}, x, y, z, t, CO₂, HCO₃, CO₃, OH, BOH₃, BOH₄, T, S) #, H)
     
     K1 = K_1(T, S)
     K2 = K_2(T, S)
@@ -195,28 +195,10 @@ end
     H = H_qss(a1, b1, a3, b3, a5, b5, CO₂, HCO₃, CO₃, OH)
     if isnan(CO₃) error("CO₃ concentration is NaN") end
     dcdt = (b3 + a4 * OH + b7 * BOH₄) * HCO₃ - (a3 * H + b4 + a7 * BOH₃) * CO₃
-    return dcdt
+    return dcdt # converting to micromol/kg rate
 end
 
-@inline function (bgc::CarbonateChemistry)(::Val{:H}, x, y, z, t, CO₂, HCO₃, CO₃, H, OH, BOH₃, BOH₄, T, S)
-    K1 = K_1(T, S)
-    K2 = K_2(T, S)
-    Kw = K_w(T, S)
-
-    a1 = alpha1(T)
-    b1 = beta1(a1, K1)
-    a3 = bgc.alpha3
-    b3 = beta3(a3, K2)
-    a5 = bgc.alpha5
-    b5 = beta5(a5, Kw)
-    #println("a5 = ", a5, " b5 = ", b5)
-    if isnan(H) error("H concentration is NaN") end
-    dcdt = a1 * CO₂ - (b1 * H - b3) * HCO₃ - a3 * H * CO₃ + (a5 - b5 * H * OH)
-    return dcdt
-end
-
-
-@inline function (bgc::CarbonateChemistry)(::Val{:OH}, x, y, z, t, CO₂, HCO₃, CO₃, H, OH, BOH₃, BOH₄, T, S)
+@inline function (bgc::CarbonateChemistry)(::Val{:OH}, x, y, z, t, CO₂, HCO₃, CO₃, OH, BOH₃, BOH₄, T, S) #, H)
     K1 = K_1(T, S)
     K2 = K_2(T, S)
     Kw = K_w(T, S)
@@ -239,10 +221,10 @@ end
     #println("a6 = ", a6, " b6 = ", b6)
     if isnan(OH) error("OH concentration is NaN") end
     dcdt = - a2 * OH * CO₂ + (b2 - a4 * OH) * HCO₃ + b4 * CO₃ + (a5 - b5 * H * OH) - (a6 * OH * BOH₃ - b6 * BOH₄)
-    return dcdt
+    return dcdt # converting to micromol/kg rate
 end
 
-@inline function (bgc::CarbonateChemistry)(::Val{:BOH₃}, x, y, z, t, CO₂, HCO₃, CO₃, H, OH, BOH₃, BOH₄, T, S)
+@inline function (bgc::CarbonateChemistry)(::Val{:BOH₃}, x, y, z, t, CO₂, HCO₃, CO₃, OH, BOH₃, BOH₄, T, S) #, H)
     K2 = K_2(T, S)
     Kw = K_w(T, S)
     Kb = K_b(T, S)
@@ -256,7 +238,7 @@ end
     if isnan(BOH₃) error("BOH₃ concentration is NaN") end
     if isnan(BOH₄) error("BOH₄ concentration is NaN") end
     dcdt = b7 * BOH₄ * HCO₃ - a7 * BOH₃ * CO₃ - (a6 * OH * BOH₃ - b6 * BOH₄)
-    return dcdt
+    return dcdt # converting to micromol/kg rate
 end
 
 @inline (bgc::CarbonateChemistry)(::Val{:BOH₄}, args...) = -bgc(Val(:BOH₃), args...)
