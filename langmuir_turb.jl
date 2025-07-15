0a62e6496c6cd9824c1a2b1fc347c9b270070d66
anpa5279 <anpa5279@colorado.edu>
2025-07-14 20:05:26 -0400

    getting ready for long runs for paper

--- langmuir_turb.jl
+++ langmuir_turb.jl
@@ -4,8 +4,8 @@ using CUDA
 using Statistics
 using Printf
 using Random
-#Pkg.develop(path="/glade/work/apauls/.julia/dev/Oceananigans.jl-main") #this will call for my version of Oceananigans locally 
-#Pkg.develop(path="/glade/work/apauls/.julia/dev/OceanBioME.jl-main") #this will call for my version of OceanBioME locally
+Pkg.develop(path="/glade/work/apauls/.julia/dev/Oceananigans.jl-main") #this will call for my version of Oceananigans locally 
+Pkg.develop(path="/glade/work/apauls/.julia/dev/OceanBioME.jl-main") #this will call for my version of OceanBioME locally
 using Oceananigans
 using OceanBioME
 using Oceananigans.Units: minute, minutes, hours, seconds
@@ -26,9 +26,10 @@ const N² = 5.3e-9    # s⁻², initial and bottom buoyancy gradient
 const initial_mixed_layer_depth = 30.0 # m 
 const Q = 0.0     # W m⁻², surface heat flux. cooling is positive
 const cᴾ = 4200.0    # J kg⁻¹ K⁻¹, specific heat capacity of seawater
-const ρₒ = 1000.0    # kg m⁻³, average density at the surface of the world ocean
+const ρₒ = 1026.0    # kg m⁻³, average density at the surface of the world ocean
 const dTdz = 0.01  # K m⁻¹, temperature gradient
-const T0 = 25.0    # C, temperature at the surface   
+const T0 = 25.0    # C, temperature at the surface  
+const S0 = 35.0    # ppt, salinity 
 const β = 2.0e-4     # 1/K, thermal expansion coefficient
 const u₁₀ = 5.75   # (m s⁻¹) wind speed at 10 meters above the ocean
 const La_t = 0.3  # Langmuir turbulence number
@@ -37,10 +38,10 @@ const La_t = 0.3  # Langmuir turbulence number
 include("stokes.jl")
 
 # Automatically distribute among available processors
-arch = Distributed(GPU())
-rank = arch.local_rank
-Nranks = MPI.Comm_size(arch.communicator)
-println("Hello from process $rank out of $Nranks")
+arch = GPU()#Distributed(GPU())
+#rank = arch.local_rank
+#Nranks = MPI.Comm_size(arch.communicator)
+#println("Hello from process $rank out of $Nranks")
 
 #grid = RectilinearGrid(; size=(Nx, Ny, Nz), extent=(Lx, Ly, Lz))
 grid = RectilinearGrid(arch; size=(Nx, Ny, Nz), extent=(Lx, Ly, Lz))
@@ -54,14 +55,16 @@ set!(dusdz, z -> dstokes_dz(z, u₁₀))
 
 u_f = La_t^2 * (stokes_velocity(-grid.z.Δᵃᵃᶜ/2, u₁₀)[1])
 τx = -(u_f^2)
-u_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(τx))
+u_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(τx), bottom = GradientBoundaryCondition(0.0)) 
+#v_bcs = FieldBoundaryConditions(top = GradientBoundaryCondition(0.0), bottom = GradientBoundaryCondition(0.0))
+#w_bcs = FieldBoundaryConditions(top = GradientBoundaryCondition(0.0), bottom = ValueBoundaryCondition(0.0)) 
 
-buoyancy = SeawaterBuoyancy(equation_of_state=LinearEquationOfState(thermal_expansion = 2e-4), constant_salinity = 35.0)
+buoyancy = SeawaterBuoyancy(equation_of_state=LinearEquationOfState(thermal_expansion = β), constant_salinity = S0)
 
-T_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(Q / (cᴾ * ρₒ * Lx * Ly)),
-                                bottom = GradientBoundaryCondition(dTdz))
+T_bcs = FieldBoundaryConditions(top = GradientBoundaryCondition(0.0),#FluxBoundaryCondition(Q / (cᴾ * ρₒ * Lx * Ly)),
+                                bottom = GradientBoundaryCondition(0.0))
 coriolis = FPlane(f=1e-4) # s⁻¹
-biogeochemistry = NPZD(; grid) #CarbonateChemistry(; grid, scale_negatives = true)
+biogeochemistry = CarbonateChemistry(; grid, scale_negatives = true)
 @show biogeochemistry
 #DIC_bcs = FieldBoundaryConditions(top = GasExchange(; gas = :CO₂, temperature = (args...) -> T0, salinity = (args...) -> 35))
 
@@ -79,39 +82,40 @@ model = NonhydrostaticModel(; grid, buoyancy, coriolis,
 r_xy(a) = randn(Xoshiro(1234), 3 * Nx)[Int(1 + round((Nx) * a/(Lx + grid.Δxᶜᵃᵃ)))]
 r_z(z) = randn(Xoshiro(1234), Nz +1)[Int(1 + round((Nz) * z/(-Lz)))] * exp(z/4)
 Tᵢ(x, y, z) = z > - initial_mixed_layer_depth ? T0 : T0 + dTdz * (z + initial_mixed_layer_depth)+ dTdz * model.grid.Lz * 1e-6 * r_z(z) * r_xy(y) * r_xy(x + Lx)
-uᵢ(x, y, z) = u_f * 1e-1 * r_z(z) * r_xy(y) * r_xy(x + Lx)
-wᵢ(x, y, z) = u_f * 1e-1 * r_z(z) * r_xy(y) * r_xy(x + Lx)
+#uᵢ(x, y, z) = u_f * 1e-1 * r_z(z) * r_xy(y) * r_xy(x + Lx)
+#wᵢ(x, y, z) = u_f * 1e-1 * r_z(z) * r_xy(y) * r_xy(x + Lx)
 perturb = 1e3
-set!(model, u=uᵢ, w=wᵢ, N=1, P=1, Z=1, D=1, T=Tᵢ, S = 35)
-#set!(model, u=uᵢ, w=wᵢ, BOH₃ = 2.97e2, BOH₄ = 1.19e2, CO₂ = 7.57e0 * perturb, CO₃ = 3.15e2, HCO₃ = 1.67e3, OH = 9.6e0, T=Tᵢ, S = 35)
+#set!(model, u=uᵢ, w=wᵢ, N=1, P=1, Z=1, D=1, T=Tᵢ, S = 35)
+set!(model, BOH₃ = 2.97e2, BOH₄ = 1.19e2, CO₂ = 7.57e0 * perturb, CO₃ = 3.15e2, HCO₃ = 1.67e3, OH = 9.6e0, T=Tᵢ, S = S0) #u=uᵢ, w=wᵢ, 
 
-simulation = Simulation(model, Δt=30, stop_time = 96hours) #stop_time = 96hours,
+
+simulation = Simulation(model, Δt=30, stop_time = 7 * 24 hours) #stop_time = 96hours,
 @show simulation
 
 function progress(simulation)
     u, v, w = simulation.model.velocities
 
     # Print a progress message
-    msg = @sprintf("i: %04d, t: %s, Δt: %s, umax = (%.1e, %.1e, %.1e) ms⁻¹, wall time: %s\n",
-    #co2 = %.1e, co3 = %.1e, hco3 = %.1e, oh = %.1e, boh3 = %.1e, boh4 = %.1e",
+    msg = @sprintf("i: %04d, t: %s, Δt: %s, umax = (%.1e, %.1e, %.1e) ms⁻¹, wall time: %s\n
+    co2 = %.1e, co3 = %.1e, hco3 = %.1e, oh = %.1e, boh3 = %.1e, boh4 = %.1e",
                    iteration(simulation),
                    prettytime(time(simulation)),
                    prettytime(simulation.Δt),
                    maximum(abs, u), maximum(abs, v), maximum(abs, w),
-                   prettytime(simulation.run_wall_time))#, 
-                   #mean(simulation.model.tracers.CO₂),
-                   #mean(simulation.model.tracers.CO₃),
-                   #mean(simulation.model.tracers.HCO₃),
-                   #mean(simulation.model.tracers.OH),
-                   #mean(simulation.model.tracers.BOH₃),
-                   #mean(simulation.model.tracers.BOH₄))
+                   prettytime(simulation.run_wall_time), 
+                   mean(simulation.model.tracers.CO₂),
+                   mean(simulation.model.tracers.CO₃),
+                   mean(simulation.model.tracers.HCO₃),
+                   mean(simulation.model.tracers.OH),
+                   mean(simulation.model.tracers.BOH₃),
+                   mean(simulation.model.tracers.BOH₄))
 
     @info msg
 
     return nothing
 end
 
-simulation.callbacks[:progress] = Callback(progress, IterationInterval(1))
+simulation.callbacks[:progress] = Callback(progress, IterationInterval(1000))
 
 conjure_time_step_wizard!(simulation, cfl=0.5, max_Δt=30.0seconds)
 
@@ -132,19 +136,19 @@ N = model.tracers.N
 P = model.tracers.P
 Z = model.tracers.Z
 D = model.tracers.D
-#BOH₃ = model.tracers.BOH₃
-#BOH₄ = model.tracers.BOH₄
-#CO₂ = model.tracers.CO₂
-#CO₃ = model.tracers.CO₃
-#HCO₃ = model.tracers.HCO₃
-#OH = model.tracers.OH
+BOH₃ = model.tracers.BOH₃
+BOH₄ = model.tracers.BOH₄
+CO₂ = model.tracers.CO₂
+CO₃ = model.tracers.CO₃
+HCO₃ = model.tracers.HCO₃
+OH = model.tracers.OH
 T = model.tracers.T
 W = Average(w, dims=(1, 2))
 U = Average(u, dims=(1, 2))
 V = Average(v, dims=(1, 2))
 T = Average(T, dims=(1, 2))
 
-simulation.output_writers[:fields] = JLD2Writer(model, (; u, v, w, N, P, Z, D), #BOH₃, BOH₄, CO₂, CO₃, HCO₃, OH
+simulation.output_writers[:fields] = JLD2Writer(model, (; u, v, w, BOH₃, BOH₄, CO₂, CO₃, HCO₃, OH),
                                                       schedule = TimeInterval(output_interval),
                                                       filename = "langmuir_turbulence_fields.jld2", #$(rank)
                                                       overwrite_existing = true,
@@ -154,6 +158,6 @@ simulation.output_writers[:averages] = JLD2Writer(model, (; U, V, W, T),
                                                     schedule = AveragedTimeInterval(output_interval, window=output_interval),
                                                     filename = "langmuir_turbulence_averages.jld2",
                                                     overwrite_existing = true)
-#simulation.output_writers[:checkpointer] = Checkpointer(model, schedule=IterationInterval(3000000), prefix="model_checkpoint")
+simulation.output_writers[:checkpointer] = Checkpointer(model, schedule=IterationInterval(30000), prefix="model_checkpoint")
 
 run!(simulation)#; pickup = true)
