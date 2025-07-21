bdd3b1f4ef9098a1ab8f304b691820677dbfe9d8
anpa5279 <anpa5279@colorado.edu>
2025-07-21 15:12:30 -0600

    wow this was frustrating.

--- langmuir_turb.jl
+++ langmuir_turb.jl
@@ -3,84 +3,85 @@ using MPI
 using CUDA
 using Statistics
 using Printf
+using Random
 using Oceananigans
-using Oceananigans.DistributedComputations
 using Oceananigans.Units: minute, minutes, hours, seconds
 using Oceananigans.BuoyancyFormulations: g_Earth
-
-mutable struct Params
-    Nx::Int         # number of points in each of x direction
-    Ny::Int         # number of points in each of y direction
-    Nz::Int         # number of points in the vertical direction
-    Lx::Float64     # (m) domain horizontal extents
-    Ly::Float64     # (m) domain horizontal extents
-    Lz::Float64     # (m) domain depth 
-    N²::Float64     # s⁻², initial and bottom buoyancy gradient
-    initial_mixed_layer_depth::Float64 # m 
-    Q::Float64      # W m⁻², surface heat flux. cooling is positive
-    cᴾ::Float64     # J kg⁻¹ K⁻¹, specific heat capacity of seawater
-    ρₒ::Float64     # kg m⁻³, average density at the surface of the world ocean
-    dTdz::Float64   # K m⁻¹, temperature gradient
-    T0::Float64     # C, temperature at the surface   
-    β::Float64      # 1/K, thermal expansion coefficient
-    u₁₀::Float64    # (m s⁻¹) wind speed at 10 meters above the ocean
-    La_t::Float64   # Langmuir turbulence number
-end
-
-#defaults, these can be changed directly below 128, 128, 160, 320.0, 320.0, 96.0
-p = Params(128, 128, 160, 320.0, 320.0, 96.0, 5.3e-9, 33.0, 0.0, 4200.0, 1000.0, 0.01, 17.0, 2.0e-4, 5.75, 0.3)
+using Oceananigans.DistributedComputations
+const Nx = 128        # number of points in each of x direction
+const Ny = 128        # number of points in each of y direction
+const Nz = 128        # number of points in the vertical direction
+const Lx = 320    # (m) domain horizontal extents
+const Ly = 320    # (m) domain horizontal extents
+const Lz = 96    # (m) domain depth 
+const N² = 5.3e-9    # s⁻², initial and bottom buoyancy gradient
+const initial_mixed_layer_depth = 30.0 # m 
+const Q = 0.0     # W m⁻², surface heat flux. cooling is positive
+const cᴾ = 4200.0    # J kg⁻¹ K⁻¹, specific heat capacity of seawater
+const ρₒ = 1026.0    # kg m⁻³, average density at the surface of the world ocean
+const dTdz = 0.01  # K m⁻¹, temperature gradient
+const T0 = 25.0    # C, temperature at the surface  
+const S0 = 35.0    # ppt, salinity 
+const β = 2.0e-4     # 1/K, thermal expansion coefficient
+const u₁₀ = 5.75   # (m s⁻¹) wind speed at 10 meters above the ocean
+const La_t = 0.3  # Langmuir turbulence number
 
 #referring to files with desiraed functions
 include("stokes.jl")
-#include("fluctuations.jl")
 
 # Automatically distribute among available processors
-arch = Distributed(GPU())
-rank = arch.local_rank
-Nranks = MPI.Comm_size(arch.communicator)
-println("Hello from process $rank out of $Nranks")
+MPI.Init() # Initialize MPI
+Nranks = MPI.Comm_size(MPI.COMM_WORLD)
+arch = Nranks > 1 ? Distributed(GPU()) : GPU()
 
-grid = RectilinearGrid(arch; size=(p.Nx, p.Ny, p.Nz), extent=(p.Lx, p.Ly, p.Lz))
+# Determine rank safely depending on architecture
+rank = arch isa Distributed ? arch.local_rank : 0
+Nranks = arch isa Distributed ? MPI.Comm_size(arch.communicator) : 1
+
+grid = RectilinearGrid(arch; size=(Nx, Ny, Nz), extent=(Lx, Ly, Lz))
 
 #stokes drift
-z_d = collect(-p.Lz + grid.z.Δᵃᵃᶜ/2 : grid.z.Δᵃᵃᶜ : -grid.z.Δᵃᵃᶜ/2)
+dusdz = Field{Nothing, Nothing, Center}(grid)
+Nx_local, Ny_local, Nz_local = size(dusdz)
+z1d = grid.z.cᵃᵃᶜ[1:Nz_local]
+dusdz_1d = dstokes_dz.(z1d, u₁₀)
+set!(dusdz, dusdz_1d)
+us = Field{Nothing, Nothing, Center}(grid)
+us_1d = stokes_velocity.(z1d, u₁₀)
+set!(us, us_1d)
+@show dusdz
 
-us = stokes_velocity(z_d, p.u₁₀)
-new_us = Field{Nothing, Nothing, Center}(grid)
-set!(new_us, reshape(us, 1, 1, :))
-dudz = dstokes_dz(z_d, p.u₁₀)
-new_dUSDdz = Field{Nothing, Nothing, Center}(grid)
-set!(new_dUSDdz, reshape(dudz, 1, 1, :))
-@show new_dUSDdz
-
-u_f = p.La_t^2 * (stokes_velocity(-grid.z.Δᵃᵃᶜ/2, p.u₁₀)[1])
+#BCs
+u_f = La_t^2 * (stokes_velocity(-grid.z.Δᵃᵃᶜ/2, u₁₀)[1])
 τx = -(u_f^2)
-u_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(τx))
+u_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(τx), bottom = GradientBoundaryCondition(0.0)) 
+v_bcs = FieldBoundaryConditions(bottom = GradientBoundaryCondition(0.0))
+w_bcs = FieldBoundaryConditions(top = ValueBoundaryCondition(0.0), bottom = GradientBoundaryCondition(0.0)) 
 
-buoyancy = SeawaterBuoyancy(equation_of_state=LinearEquationOfState(thermal_expansion = 2e-4), constant_salinity = 35.0)
-T_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(p.Q / (p.cᴾ * p.ρₒ * p.Lx * p.Ly)),
-                                bottom = GradientBoundaryCondition(p.dTdz))
-#coriolis = FPlane(f=1e-4) # s⁻¹
+buoyancy = SeawaterBuoyancy(equation_of_state=LinearEquationOfState(thermal_expansion = β), constant_salinity = S0)
 
-model = NonhydrostaticModel(; grid, buoyancy, #coriolis,
+T_bcs = FieldBoundaryConditions(top = GradientBoundaryCondition(0.0),#FluxBoundaryCondition(Q / (cᴾ * ρₒ * Lx * Ly)),
+                                bottom = GradientBoundaryCondition(0.0))
+coriolis = FPlane(f=1e-4) # s⁻¹
+
+model = NonhydrostaticModel(; grid, buoyancy, coriolis,
                             advection = WENO(),
+                            tracers = (:T,),
                             timestepper = :RungeKutta3,
-                            tracers = (:T),
-                            closure = Smagorinsky(coefficient=0.1), #AnisotropicMinimumDissipation(),
-                            stokes_drift = UniformStokesDrift(∂z_uˢ=new_dUSDdz),
-                            boundary_conditions = (u=u_bcs, T=T_bcs)) 
+                            closure = Smagorinsky(coefficient=0.1),
+                            stokes_drift = UniformStokesDrift(∂z_uˢ=dusdz),
+                            boundary_conditions = (u=u_bcs, v=v_bcs, w=w_bcs, T=T_bcs))#, CO₂ = DIC_bcs)) 
 @show model
 
-# random seed
-Ξ(z) = randn() * exp(z / 4)
+# ICs
+r_z(z) = randn(Xoshiro()) * exp(z/4)
+Tᵢ(x, y, z) = z > - initial_mixed_layer_depth ? T0 : T0 + dTdz * (z + initial_mixed_layer_depth)+ dTdz * model.grid.Lz * 1e-6 * r_z(z) 
+uᵢ(x, y, z) = u_f * 1e-1 * r_z(z) 
+vᵢ(x, y, z) = -u_f * 1e-1 * r_z(z) 
+set!(model, u=uᵢ, v=vᵢ, T=Tᵢ)
 
-Tᵢ(x, y, z) = z > - p.initial_mixed_layer_depth ? p.T0 : p.T0 + p.dTdz * (z + p.initial_mixed_layer_depth)+ p.dTdz * model.grid.Lz * 1e-6 * Ξ(z)
-vᵢ(x, y, z) = u_f * 1e-1 * Ξ(z)
-wᵢ(x, y, z) = u_f * 1e-1 * Ξ(z)
-
-set!(model, u=new_us, v=vᵢ, w=wᵢ, T=Tᵢ)
-
-simulation = Simulation(model, Δt=30.0, stop_time = 96hours) #stop_time = 96hours,
+day = 24hours
+simulation = Simulation(model, Δt=30, stop_time = 4*day) #stop_time = 96hours,
 @show simulation
 
 function progress(simulation)
@@ -99,38 +100,39 @@ function progress(simulation)
     return nothing
 end
 
-simulation.callbacks[:progress] = Callback(progress, IterationInterval(20))
+simulation.callbacks[:progress] = Callback(progress, IterationInterval(5000))
 
-conjure_time_step_wizard!(simulation, cfl=0.5, max_Δt=30seconds)
+conjure_time_step_wizard!(simulation, cfl=0.5, max_Δt=30.0seconds)
 
 #output files
 function save_IC!(file, model)
-    if rank == 0
-        file["IC/friction_velocity"] = u_f
-        file["IC/stokes_velocity"] = stokes_velocity(-grid.z.Δᵃᵃᶜ/2, p.u₁₀)[1]
-        file["IC/wind_speed"] = p.u₁₀
-    end
+    #if rank == 0
+    file["IC/friction_velocity"] = u_f
+    file["IC/stokes_velocity"] = stokes_velocity(-grid.z.Δᵃᵃᶜ/2, u₁₀)[1]
+    file["IC/wind_speed"] = u₁₀
+    #end
     return nothing
 end
 
-output_interval = 60minutes
+output_interval = 6hours
 
 u, v, w = model.velocities
+T = model.tracers.T
 W = Average(w, dims=(1, 2))
 U = Average(u, dims=(1, 2))
 V = Average(v, dims=(1, 2))
-T = Average(model.tracers.T, dims=(1, 2))
+T_avg = Average(T, dims=(1, 2))
 
-simulation.output_writers[:fields] = JLD2OutputWriter(model,  (; u, w),
+simulation.output_writers[:fields] = JLD2Writer(model, (; u, v, w),
                                                       schedule = TimeInterval(output_interval),
-                                                      filename = "outputs/langmuir_turbulence_fields.jld2", #$(rank)
+                                                      filename = "langmuir_turbulence_fields.jld2", #$(rank)
                                                       overwrite_existing = true,
                                                       init = save_IC!)
                                                       
-simulation.output_writers[:averages] = JLD2OutputWriter(model, (; U, V, W, T),
+simulation.output_writers[:averages] = JLD2Writer(model, (; U, V, W, T_avg),
                                                     schedule = AveragedTimeInterval(output_interval, window=output_interval),
-                                                    filename = "outputs/langmuir_turbulence_averages.jld2",
+                                                    filename = "langmuir_turbulence_averages.jld2",
                                                     overwrite_existing = true)
-#simulation.output_writers[:checkpointer] = Checkpointer(model, schedule=IterationInterval(6.8e4), prefix="model_checkpoint_$(rank)")
+simulation.output_writers[:checkpointer] = Checkpointer(model, schedule=IterationInterval(30000), prefix="model_checkpoint")
 
 run!(simulation)#; pickup = true)
