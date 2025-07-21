d6048095dc498eb42b5a7b782543f84d0ef9d782
anpa5279 <anpa5279@colorado.edu>
2025-07-21 15:35:14 -0600

    removing stokes profile

--- langmuir_turb.jl
+++ langmuir_turb.jl
@@ -46,9 +46,9 @@ Nx_local, Ny_local, Nz_local = size(dusdz)
 z1d = grid.z.cᵃᵃᶜ[1:Nz_local]
 dusdz_1d = dstokes_dz.(z1d, u₁₀) .* ones(Nx_local, Ny_local, Nz_local)
 set!(dusdz, dusdz_1d)
-us = Field{Nothing, Nothing, Center}(grid)
-us_1d = stokes_velocity.(z1d, u₁₀)
-set!(us, us_1d)
+#us = Field{Nothing, Nothing, Center}(grid)
+#us_1d = stokes_velocity.(z1d, u₁₀)
+#set!(us, us_1d)
 @show dusdz
 
 #BCs
