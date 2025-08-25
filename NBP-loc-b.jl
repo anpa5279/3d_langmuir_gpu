47fd7d8dba5fb0d9a62785b6aede614a47a56c62
anpa5279 <anpa5279@colorado.edu>
2025-08-25 14:19:56 -0600

    no sgs testing

--- NBP-loc-b.jl
+++ NBP-loc-b.jl
@@ -47,7 +47,7 @@ model = NonhydrostaticModel(; grid, buoyancy, coriolis,
                             advection = WENO(),
                             tracers = (:b,),
                             timestepper = :RungeKutta3,
-                            closure = Smagorinsky(), 
+                            #closure = Smagorinsky(), 
                             stokes_drift = UniformStokesDrift(∂z_uˢ=dusdz),
                             boundary_conditions = (u=u_bcs, b=b_bcs))
 @show model
