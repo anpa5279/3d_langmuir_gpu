f5c5c98be61b3412d457605cad5a472b036f91c1
anpa5279 <anpa5279@colorado.edu>
2025-11-17 15:20:58 -0700

    pushing latest edits

--- langmuir_turb-local.jl
+++ langmuir_turb-local.jl
@@ -1,16 +1,14 @@
 using Pkg
-using MPI
-using CUDA
 using Statistics
 using Printf
 using Random
 using Oceananigans
 using Oceananigans.Units: minute, minutes, hours, seconds
 using Oceananigans: defaults #using Oceananigans.BuoyancyFormulations: g_Earth
-using Oceananigans.DistributedComputations
-const Nx = 128        # number of points in each of x direction
-const Ny = 128        # number of points in each of y direction
-const Nz = 128        # number of points in the vertical direction
+using Oceananigans.TurbulenceClosures: Smagorinsky
+const Nx = 8        # number of points in each of x direction
+const Ny = 8        # number of points in each of y direction
+const Nz = 8        # number of points in the vertical direction
 const Lx = 320    # (m) domain horizontal extents
 const Ly = 320    # (m) domain horizontal extents
 const Lz = 96    # (m) domain depth 
@@ -25,16 +23,7 @@ const β = 2.0e-4     # 1/K, thermal expansion coefficient
 const u₁₀ = 5.75   # (m s⁻¹) wind speed at 10 meters above the ocean
 const La_t = 0.3  # Langmuir turbulence number
 
-# Automatically distribute among available processors
-MPI.Init() # Initialize MPI
-Nranks = MPI.Comm_size(MPI.COMM_WORLD)
-arch = Nranks > 1 ? Distributed(GPU()) : GPU()
-
-# Determine rank safely depending on architecture
-rank = arch isa Distributed ? arch.local_rank : 0
-Nranks = arch isa Distributed ? MPI.Comm_size(arch.communicator) : 1
-
-grid = RectilinearGrid(arch; size=(Nx, Ny, Nz), extent=(Lx, Ly, Lz)) #arch
+grid = RectilinearGrid(; size=(Nx, Ny, Nz), extent=(Lx, Ly, Lz)) #arch
 
 #stokes drift
 g_Earth = defaults.gravitational_acceleration
@@ -70,7 +59,8 @@ model = NonhydrostaticModel(; grid, buoyancy, #coriolis,
 # ICs
 r_z(z) = randn(Xoshiro()) * exp(z/4)
 Tᵢ(x, y, z) = z > - initial_mixed_layer_depth ? T0 : T0 + dTdz * (z + initial_mixed_layer_depth)+ dTdz * model.grid.Lz * 1e-6 * r_z(z) 
-uᵢ(x, y, z) = u_f * r_z(z) 
+uᵢ(x, y, z) = -u_f * r_z(z) 
+vᵢ(x, y, z) = -uᵢ(x, y, z)
 set!(model, u=uᵢ, w=0.0, T=Tᵢ)#v=vᵢ, 
 
 day = 24hours
@@ -99,11 +89,9 @@ conjure_time_step_wizard!(simulation, IterationInterval(1); cfl=0.5, max_Δt=30s
 
 #output files
 function save_IC!(file, model)
-    if rank == 0 || Nranks == 1
-        file["IC/friction_velocity"] = u_f
-        file["IC/stokes_velocity"] = us
-        file["IC/wind_speed"] = u₁₀
-    end
+    file["IC/friction_velocity"] = u_f
+    file["IC/stokes_velocity"] = us
+    file["IC/wind_speed"] = u₁₀
     return nothing
 end
 
@@ -120,13 +108,11 @@ simulation.output_writers[:fields] = JLD2Writer(model, (; u, v, w, T),
                                                     schedule = TimeInterval(output_interval),
                                                     filename = "langmuir_turbulence_fields.jld2", #$(rank)
                                                     overwrite_existing = true,
-                                                    array_type = Array{Float64},
                                                     init = save_IC!)
                                                       
 simulation.output_writers[:averages] = JLD2Writer(model, (; U, V, W, T_avg),
                                                     schedule = AveragedTimeInterval(output_interval, window=output_interval),
                                                     filename = "langmuir_turbulence_averages.jld2",
-                                                    array_type = Array{Float64},
                                                     overwrite_existing = true)
 #simulation.output_writers[:checkpointer] = Checkpointer(model, schedule=IterationInterval(30000), prefix="model_checkpoint")
 
