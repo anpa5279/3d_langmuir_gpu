eca3f7311a14dde47d13cd57b577df6cadb74963
anpa5279 <anpa5279@colorado.edu>
2025-12-04 12:17:02 -0700

    run 13 c ran for longer and hopefully runs on latest oceananigans

--- langmuir_turb.jl
+++ langmuir_turb.jl
@@ -8,6 +8,7 @@ using Oceananigans
 using Oceananigans.Units: minute, minutes, hours, seconds
 using Oceananigans: defaults #using Oceananigans.BuoyancyFormulations: g_Earth
 using Oceananigans.DistributedComputations
+using Oceananigans.TurbulenceClosures: Smagorinsky
 const Nx = 128        # number of points in each of x direction
 const Ny = 128        # number of points in each of y direction
 const Nz = 128        # number of points in the vertical direction
