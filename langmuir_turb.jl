60ea8d3ddc6e64969bc7c2c13bec171f7beaf65d
anpa5279 <anpa5279@colorado.edu>
2025-12-11 12:52:03 -0700

    corrected for derecho run

--- langmuir_turb.jl
+++ langmuir_turb.jl
@@ -66,7 +66,7 @@ v_bcs = FieldBoundaryConditions(top = GradientBoundaryCondition(0.0), #ValueBoun
                                 bottom = GradientBoundaryCondition(0.0))
 
 model = NonhydrostaticModel(; grid, coriolis,
-                            advection = WENO(order=5),
+                            #advection = WENO(order=5),
                             timestepper = :RungeKutta3,
                             tracers = :T,
                             buoyancy = buoyancy,
