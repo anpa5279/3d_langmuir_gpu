9455109b66bcb18f290c172ec27fb707025697f4
anpa5279 <anpa5279@colorado.edu>
2025-12-10 18:08:26 -0700

    testing smaller initial time step

--- langmuir_turb.jl
+++ langmuir_turb.jl
@@ -97,7 +97,7 @@ set!(Tᵢ1, T_i1)
 
 set!(model, w=0.0, u=uᵢ, v=vᵢ, T=Tᵢ) #u=u_i1, v=v_i1, T=T_i1) #
 @show "ICs set"
-simulation = Simulation(model, Δt=30.0, stop_time=240*hours)
+simulation = Simulation(model, Δt=15.0, stop_time=240*hours)
 @show simulation
 
 function progress(simulation)
@@ -118,7 +118,7 @@ end
 
 simulation.callbacks[:progress] = Callback(progress, IterationInterval(1000))
 
-conjure_time_step_wizard!(simulation, IterationInterval(1); cfl=0.5, max_Δt=30seconds)
+conjure_time_step_wizard!(simulation, IterationInterval(1); cfl=0.5, max_Δt=15seconds)
 
 #output files
 function save_IC!(file, model)
