b48a25fddac4744148c432bee591c9c8893b35a8
anpa5279 <anpa5279@colorado.edu>
2025-12-14 21:13:17 -0600

    new ICs. baseline with amd

--- langmuir_turb.jl
+++ langmuir_turb.jl
@@ -75,7 +75,7 @@ model = NonhydrostaticModel(; grid, coriolis,
                             timestepper = :RungeKutta3,
                             tracers = :T,
                             buoyancy = buoyancy,
-                            closure = Smagorinsky(coefficient=0.1), #AnisotropicMinimumDissipation(), #
+                            closure = AnisotropicMinimumDissipation(), #Smagorinsky(coefficient=0.1), #
                             stokes_drift = UniformStokesDrift(∂z_uˢ=∂z_uˢ),
                             boundary_conditions = (u=u_bcs, v=v_bcs, T=T_bcs)
                             )
