47c668cc9864e0f74b5bcd77f7c3ad464d9e8627
anpa5279 <anpa5279@colorado.edu>
2025-12-18 03:28:00 -0600

    ugh

--- langmuir_turb-local-bgc.jl
+++ langmuir_turb-local-bgc.jl
@@ -11,9 +11,9 @@ Pkg.develop(path="/Users/annapauls/Documents/Github repositories/personal_oceana
 using OceanBioME: CarbonateChemistry, carbonate_thermo_kernel
 using OceanBioME: ContinuousBiogeochemistry
 
-const Nx = 32        # number of points in each of x direction
-const Ny = 32        # number of points in each of y direction
-const Nz = 32        # number of points in the vertical direction
+const Nx = 2        # number of points in each of x direction
+const Ny = 2        # number of points in each of y direction
+const Nz = 2        # number of points in the vertical direction
 const Lx = 320    # (m) domain horizontal extents
 const Ly = 320    # (m) domain horizontal extents
 const Lz = 96    # (m) domain depth 
