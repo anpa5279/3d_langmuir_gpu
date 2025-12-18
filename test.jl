b6b07eb79da91df01b2cc03a99d96f5823ed2505
anpa5279 <anpa5279@colorado.edu>
2025-12-17 21:25:51 -0600

    testing

--- test.jl
+++ test.jl
@@ -27,7 +27,7 @@ function test_CarbonateChemistry(grid, sinking, open_bottom)
     @test all([all(Array(interior(values)) .== 0) for values in values(model.tracers)]) 
 
     # mass conservation
-    set!(model, BOH3 = 2.97e2, BOH4 = 1.19e2, CO2 = 7.57e0 * perturb, CO3 = 3.15e2, HCO3 = 1.67e3, OH = 9.6e0) 
+    set!(model, BOH3 = 2.97e2, BOH4 = 1.19e2, CO2 = 7.57e0 * 1.0e3, CO3 = 3.15e2, HCO3 = 1.67e3, OH = 9.6e0) 
 
     ΣN₀ = sum(Array(interior(model.tracers.BOH3))) + 
           sum(Array(interior(model.tracers.BOH4))) + 
