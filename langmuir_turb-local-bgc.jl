c79f1342d204e704de15c49f8e5b1cfc35183e93
anpa5279 <anpa5279@colorado.edu>
2025-12-18 03:54:53 -0600

    ugh

--- langmuir_turb-local-bgc.jl
+++ langmuir_turb-local-bgc.jl
@@ -134,7 +134,7 @@ function progress(simulation)
 end
 
 simulation.callbacks[:progress] = Callback(progress, IterationInterval(5000))
-simulation.callbacks[:cc_updates] = Callback(carbonate_thermo_kernel, IterationInterval(1), callsite=UpdateStateCallsite())
+simulation.callbacks[:cc_updates] = Callback(carbonate_thermo_kernel, IterationInterval(1))#, callsite=UpdateStateCallsite())
 
 output_interval =  0.1*seconds
 
