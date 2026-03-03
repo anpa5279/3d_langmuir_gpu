2efb0d66ace5a0ed7c320c718a4463de6a97d7fb
anpa5279 <anpa5279@colorado.edu>
2026-03-03 12:47:39 -0700

    prepping for comparing to buoyancy tracer case

--- T-periodicboundary-closureRe-complexity.jl
+++ T-periodicboundary-closureRe-complexity.jl
@@ -44,15 +44,17 @@ v_bcs = FieldBoundaryConditions(top = GradientBoundaryCondition(0.0),
                                 bottom = GradientBoundaryCondition(0.0))
 T_bcs = FieldBoundaryConditions(top = GradientBoundaryCondition(0.0),
                                 bottom = GradientBoundaryCondition(dTdz))
-g = Oceananigans.defaults.gravitational_acceleration
-wp = -0.001
-b0 = -4*10^(-1) # m s⁻²
-rho_ratio = (rho_tracer-rho0) / (rho0) 
-Sj = -b0/(g*rho_ratio) # amount of tracer flux needed to achieve the desired buoyancy flux
-Jᵇ = wp*Sj
+wp = 0.001
+Sj = 100.0 # kg, tracer mass
+area = 2*pi*rp^2 # m², area for tracer
+x_area = [Lx/2-rp, Lx/2+rp]
+y_area = [Ly/2-rp, Ly/2+rp]
 @inline function sflux(x, y, t) 
-    σ = 10.0 # m
-    return wp*Sj/(2*pi* σ^2) * exp(-(x-Lx/2)^2 / (2 * σ^2)) * exp(-(y-Ly/2)^2 / (2 * σ^2)) 
+    if x >= x_area[1] && x <= x_area[2] && y >= y_area[1] && y <= y_area[2]
+        return -wp*Sj
+    else
+        return 0.0
+    end
 end
 S_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(sflux), 
                                 bottom = GradientBoundaryCondition(0.0))
@@ -67,27 +69,25 @@ sgs = ScalarDiffusivity(ν=visc, κ=visc)
 beta = (rho_tracer - rho0) / (rho0 * Sj) 
 buoyancy = SeawaterBuoyancy(equation_of_state=LinearEquationOfState(thermal_expansion = alpha, haline_contraction = beta))
 
-## defining model
-model = NonhydrostaticModel(grid;
-                            buoyancy, 
-                            advection = WENO(),
-                            tracers = (:T, :S,),
-                            timestepper = :RungeKutta3,
-                            boundary_conditions = (u = u_bcs, v = v_bcs, S=S_bcs, T=T_bcs),
-                            closure = sgs
-                            )
-@show model
+# stokes drift
+g = Oceananigans.defaults.gravitational_acceleration
+include("stokes.jl")
+dusdz_top = dstokes_dz(grid.z.cᵃᵃᶜ[Nz]/2, u₁₀)
+dusdz_bot = dstokes_dz(grid.z.cᵃᵃᶜ[0], u₁₀)
+dusdz_bcs = FieldBoundaryConditions(grid, (nothing, nothing, Center()), top = ValueBoundaryCondition(dusdz_top), 
+                                bottom = ValueBoundaryCondition(dusdz_bot))
+dusdz = Field{Nothing, Nothing, Center}(grid; boundary_conditions = dusdz_bcs)
+dusdz_1d = dstokes_dz.(grid.z.cᵃᵃᶜ[1:Nz], u₁₀)
+set!(dusdz, reshape(dusdz_1d, 1, 1, :))
+us = stokes_velocity.(grid.z.cᵃᵃᶜ[1:Nz], u₁₀)
+
 ## ICs
 r(x, y, z) = (randn(Xoshiro())) * exp(z/4)
 uᵢ(x, y, z) = wp * r(x, y, z)
 vᵢ(x, y, z) = -wp * r(x, y, z)
 Tᵢ(x, y, z) = z > - MLD ? dTdz * Lz * 1e-6 * r(x, y, z) : #random noise in the mixed layer
             dTdz * (z + MLD) + dTdz * Lz * 1e-6 * r(x, y, z)
-set!(model, u=uᵢ, v=vᵢ, T=Tᵢ, S=0.0)
 
-# defining simulation
-simulation = Simulation(model, Δt=30, stop_time = 12hours) 
-@show simulation
 ## progress function
 function progress(simulation)
     u, v, w = simulation.model.velocities
@@ -101,6 +101,44 @@ function progress(simulation)
     @info msg
     return nothing
 end
+## output file inputs
+function save_IC!(file, model)
+    file["IC/friction_velocity"] = u_f
+    file["IC/stokes_velocity"] = us
+    file["IC/wind_speed"] = u₁₀
+    return nothing
+end
+
+# for loop variations 
+# Langmuir number ---> changes wind stress (u flux BC)
+#for La_t in [0.2, 0.3, 0.4]
+#uf = La_t^2 * us[Nz]
+#τx = -(uf^2)
+#u_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(τx), bottom = GradientBoundaryCondition(0.0))
+# turn stokes on or off
+# for stokes in [nothing, UniformStokesDrift(∂z_uˢ=dusdz)]
+# turn coriolis on or off
+# for coriolis in [nothing, FPlane(f=1e-4)]
+
+## defining model
+model = NonhydrostaticModel(grid;
+                            coriolis,
+                            stokes_drift = stokes,
+                            buoyancy, 
+                            advection = WENO(),
+                            tracers = (:T, :S,),
+                            timestepper = :RungeKutta3,
+                            boundary_conditions = (u = u_bcs, v = v_bcs, S=S_bcs, T=T_bcs),
+                            closure = sgs
+                            )
+@show model
+if stokes !== nothing
+    uᵢ(x, y, z) = wp * r(x, y, z) + stokes_velocity(z, u₁₀)
+end
+set!(model, u=uᵢ, v=vᵢ, T=Tᵢ, S=0.0)
+# defining simulation
+simulation = Simulation(model, Δt=30, stop_time = 12hours) 
+@show simulation
 simulation.callbacks[:progress] = Callback(progress, IterationInterval(500))
 ## updating cfl every time step
 conjure_time_step_wizard!(simulation, IterationInterval(1); cfl=0.5, diffusive_cfl = 1.0, min_Δt = 1.0, max_Δt=30seconds) #ensrues cfl is updated ever iteration
