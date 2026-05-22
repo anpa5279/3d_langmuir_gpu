000de41c2ae05559d52855e411c78ace79a9785a
anpa5279 <anpa5279@colorado.edu>
2026-05-22 08:28:12 -0600

    lx = 256, dx  = 1.5

--- T-periodicboundary.jl
+++ T-periodicboundary.jl
@@ -10,9 +10,9 @@ using Printf
 using Oceananigans
 using Oceananigans: UpdateStateCallsite
 using Oceananigans.Units: minute, minutes, hours, seconds
-Lx = Ly = 384            # (m) domain horizontal extents
+Lx = Ly = 256            # (m) domain horizontal extents
 Lz = 160             # (m) domain depth 
-Nx = Ny = Lx*1.25
+Nx = Ny = Lx*1.5
 Nz = 320
 MLD = 60.0          # m, mixed layer depth
 dTdz = 0.01       # K m⁻¹, temperature gradient
