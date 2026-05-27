9ffb20577d6148f3f1ad01b4c7f1ed2f7eb15573
anpa5279 <anpa5279@colorado.edu>
2026-05-27 12:10:53 -0600

    updating code to hopefully be faster. rerunning this case to get more time steps. lx = 640, nx = 512

--- T-periodicboundary.jl
+++ T-periodicboundary.jl
@@ -10,9 +10,9 @@ using Printf
 using Oceananigans
 using Oceananigans: UpdateStateCallsite
 using Oceananigans.Units: minute, minutes, hours, seconds
-Lx = Ly = 560            # (m) domain horizontal extents
+Lx = Ly = 640            # (m) domain horizontal extents
 Lz = 160             # (m) domain depth 
-Nx = Ny = 448
+Nx = Ny = 512
 Nz = 320
 MLD = 60.0          # m, mixed layer depth
 dTdz = 0.01       # K m⁻¹, temperature gradient
