4bf4be889ae11db025100716dac4cacf9c636b36
anpa5279 <anpa5279@colorado.edu>
2026-06-09 21:31:40 -0600

    new ground default

--- T-periodicboundary.jl
+++ T-periodicboundary.jl
@@ -14,7 +14,7 @@ using Oceananigans: UpdateStateCallsite
 using Oceananigans.Units: minute, minutes, hours, seconds
 
 Lx = Ly = 128           # (m) domain horizontal extents
-Nx = Ny = 64*2 #ensure it is only powers of 2 (maybe 3)
+Nx = Ny = 64*2^2 #ensure it is only powers of 2 (maybe 3)
 
 Lz = 128             # (m) domain depth 
 Nz = 256
