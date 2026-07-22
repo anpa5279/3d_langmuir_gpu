429d0977028ab900951b014f4f60405f4c761703
anpa5279 <anpa5279@colorado.edu>
2026-07-22 12:58:36 -0600

    dz = 0.5

--- T-periodicboundary.jl
+++ T-periodicboundary.jl
@@ -78,7 +78,7 @@ T_bcs = FieldBoundaryConditions(top = GradientBoundaryCondition(0.0),
 S_bcs = FieldBoundaryConditions(top = ValueBoundaryCondition(s_value), 
                                 bottom = GradientBoundaryCondition(0.0))
 ## defining model
-visc = 1e-4
+visc = 1e-5
 closure = ScalarDiffusivity(ν=visc, κ=visc)
 model = NonhydrostaticModel(grid;
                             buoyancy, 
