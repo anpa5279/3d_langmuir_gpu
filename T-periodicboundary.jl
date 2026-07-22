a30acd405337431982707d7c8ce806f227af598b
anpa5279 <anpa5279@colorado.edu>
2026-07-22 12:53:34 -0600

    dz = 0.025

--- T-periodicboundary.jl
+++ T-periodicboundary.jl
@@ -15,10 +15,10 @@ using Oceananigans: UpdateStateCallsite
 using Oceananigans.Units: minute, minutes, hours, seconds
 
 Lx = Ly = 64           # (m) domain horizontal extents
-Nx = Ny = 128 #ensure it is only powers of 2 (maybe 3)
+Nx = Ny = 256 #ensure it is only powers of 2 (maybe 3)
 
 Lz = 128             # (m) domain depth 
-Nz = 256
+Nz = 512
 MLD = 60.0          # m, mixed layer depth
 dTdz = 0.01       # K m⁻¹, temperature gradient
 alpha = 2.0e-4      # 1/K, thermal expansion coefficient
