60cfad09c9b6e84bbe1e9a0fea36c2e824c89807
anpa5279 <anpa5279@colorado.edu>
2026-08-04 12:17:36 -0600

    prepping 0.0625m case

--- T-periodicboundary.jl
+++ T-periodicboundary.jl
@@ -15,10 +15,10 @@ using Oceananigans: UpdateStateCallsite
 using Oceananigans.Units: minute, minutes, hours, seconds
 
 Lx = Ly = 64           # (m) domain horizontal extents
-Nx = Ny = 512 #ensure it is only powers of 2 (maybe 3)
+Nx = Ny = 1024 #ensure it is only powers of 2 (maybe 3)
 
 Lz = 128             # (m) domain depth 
-Nz = 1024
+Nz = 2048
 MLD = 60.0          # m, mixed layer depth
 dTdz = 0.01       # K m⁻¹, temperature gradient
 alpha = 2.0e-4      # 1/K, thermal expansion coefficient
